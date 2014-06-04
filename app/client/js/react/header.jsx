40c2fcc791f7c7b59ff9b42e1ee888a26ce5e70f
Daniel Steigerwald <daniel@steigerwald.cz>
2014-06-05 00:42:18 +0200

    Add some notes to header.jsx.

--- app/client/js/react/header.jsx
+++ app/client/js/react/header.jsx
@@ -2,6 +2,7 @@
 goog.provide('app.react.Header');
 
 /**
+ * This is example for Facebook React JSX.
  * @constructor
  */
 app.react.Header = function() {
@@ -10,7 +11,25 @@ app.react.Header = function() {
 
     render: function() {
       return (
-        <h3>Simple Isomorphic TodoApp Demo</h3>
+        <header>
+          <h1>Este.js demo</h1>
+          <p>
+            Simple <a href="http://nerds.airbnb.com/isomorphic-javascript-future-web-apps/" target="_blank">
+              isomorphic
+            </a>{' '}
+            TodoMVC app written with <a href="https://github.com/steida/este" target="_blank">
+              Este.js
+            </a>{' '}
+            and <a href="https://github.com/steida/este-library" target="_blank">
+              este-library
+            </a>.
+          </p>
+          <p>
+            View page source, this ajax app is server side rendered. Not only,
+            we can share any code between client and server side. This is a{' '}
+            <b>Holy Grail</b>.
+          </p>
+        </header>
       );
     }
 
