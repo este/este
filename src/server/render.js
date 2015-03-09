486994a744121354a14a99cde7e4da3d4984c71c
Desmond Brand <dmnd@desmondbrand.com>
2015-03-08 19:40:55 -0700

    Add Google Analytics
    When an id is configured it will appear at the end of the body.
    Otherwise nothing changes.

--- src/server/render.js
+++ src/server/render.js
@@ -65,6 +65,20 @@ function getPageHtml(Handler, appState) {
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(app, s);
       })();
     </script>`
+
+  const gaid = config.googleAnalyticsId;
+  if (gaid !== 'UA-XXXXXXX-X') {
+    scriptHtml += `
+      <!-- Google Analytics -->
+      <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='//www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','${gaid}');ga('send','pageview');
+      </script>`
+  }
   let title = DocumentTitle.rewind()
 
   return '<!DOCTYPE html>' + React.renderToStaticMarkup(
