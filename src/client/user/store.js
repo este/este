f0652d0a75054838bada0d6fa85701a23cf03b59
ronag <roang89@gmail.com>
2015-03-17 18:59:14 +0100

    Simplified Action Handling

--- src/client/user/store.js
+++ src/client/user/store.js
@@ -1,12 +1,7 @@
 import dispatcher from '../dispatcher'
 import {userCursor} from '../state'
 
-export const dispatchToken = dispatcher.register((payload) => {
-  const {action, data} = payload
-
-  // switch (action) {
-  // }
-
+export const dispatchToken = dispatcher.register({
 })
 
 export function getUser() {
