b3eb266745f66a3061645b3b3b4d285daee4b504
Daniel Steigerwald <daniel@steigerwald.cz>
2015-03-19 05:42:40 +0100

    Add gif and jpg to webpack url-loader.

--- webpack/makeconfig.js
+++ webpack/makeconfig.js
@@ -60,7 +60,7 @@ module.exports = function(isDevelopment) {
     module: {
       loaders: [{
         loader: 'url-loader?limit=100000',
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/
+        test: /\.(gif|jpg|png|woff|woff2|eot|ttf|svg)$/
       }, {
         exclude: /node_modules/,
         loaders: isDevelopment ? [
