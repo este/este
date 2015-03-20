42417586e58149d7e34588ceee23071d256abacc
Daniel Steigerwald <daniel@steigerwald.cz>
2015-03-20 01:46:08 +0100

    Formatting.

--- src/server/index.js
+++ src/server/index.js
@@ -2,15 +2,18 @@ const config = require('./config')
 
 if (config.isProduction || require('piping')(config.piping)) {
   if (!process.env.NODE_ENV)
-    throw new Error('Environment variable NODE_ENV must be set.')
+    throw new Error('Enviroment variable NODE_ENV must be set.')
+
   // Load and use polyfill for ECMA-402.
   if (!global.Intl)
     global.Intl = require('intl')
 
   require('babel/register')
+
   // To ignore webpack custom loaders on server.
   config.webpackStylesExtensions.forEach(function(ext) {
     require.extensions['.' + ext] = function() {}
   })
+
   require('./main')
 }
