9e7cf200ddd44589ae45e99225bb24c52e9c9531
Daniel Steigerwald <daniel@steigerwald.cz>
2015-03-23 02:46:33 +0100

    Refactor intl store and validations.

--- src/client/validation.js
+++ src/client/validation.js
@@ -1,28 +1,22 @@
-import IntlMessageFormat from 'intl-messageformat'
 import Validation from '../lib/validation'
-import {i18nCursor} from './state'
+import {msg} from './intl/store'
 
 class AppValidation extends Validation {
 
   getRequiredMessage(prop) {
-    return getMessage('required', {prop})
+    return msg('validation.required', {prop})
   }
 
   getEmailMessage(prop) {
-    return getMessage('email', {prop})
+    return msg('validation.email', {prop})
   }
 
   getSimplePasswordMessage(minLength) {
-    return getMessage('password', {minLength})
+    return msg('validation.password', {minLength})
   }
 
 }
 
 export function validate(object: Object) {
   return new AppValidation(object)
-}
-
-function getMessage(messageKey, values) {
-  const message = i18nCursor().getIn(['messages', 'validation', messageKey])
-  return new IntlMessageFormat(message).format(values)
 }
