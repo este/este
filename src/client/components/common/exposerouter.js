47a958e9804304d54f3894db4334f0ffd28c6344
ronag <roang89@gmail.com>
2015-03-24 15:05:03 +0100

    Use React.cloneElement for higher order functions.

--- src/client/components/common/exposerouter.js
+++ src/client/components/common/exposerouter.js
@@ -3,17 +3,15 @@ import React from 'react'
 // Higher order component exposing router.
 // https://medium.com/@dan_abramov/mixins-are-dead-long-live-higher-order-components-94a0d2f9e750
 export default function exposeRouter(Component) {
-
   class ExposeRouter extends React.Component {
     render() {
-      return <Component {...this.props} router={this.context.router} />
+      return React.cloneElement(React.Children.onlyChild(this.props.children), {
+        router: this.context.router
+      });
     }
   }
-
   ExposeRouter.contextTypes = {
     router: React.PropTypes.func.isRequired
   }
-
   return ExposeRouter
-
 }
