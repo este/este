b71fcdb6390d8ef5db3bbbb18da7fadeeeca0818
zigomir <zigomir@gmail.com>
2015-04-04 22:32:56 +0200

    clenup
    - don't use template strings - back ticks
    - don't use js strings - single quotes

--- src/client/components/notfound.js
+++ src/client/components/notfound.js
@@ -6,15 +6,15 @@ export default class NotFound extends React.Component {
 
   render() {
     return (
-      <DocumentTitle title={'Page Not Found'}>
+      <DocumentTitle title="Page Not Found">
         <div>
           <h1>
-            {`This page isn't available`}
+            This page isn't available
           </h1>
           <p>
-            {'The link may be broken, or the page may have been removed.'}
+            The link may be broken, or the page may have been removed.
           </p>
-          <Link to="home">{'Continue here please.'}</Link>
+          <Link to="home">Continue here please.</Link>
         </div>
       </DocumentTitle>
     );
