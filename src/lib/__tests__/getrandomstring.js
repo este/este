09fdd0035c1cd5c01eb067b04bbc49b7ab98441f
ronag <roang89@gmail.com>
2015-04-07 10:43:27 +0200

    Added comment to jest test.

--- src/lib/__tests__/getrandomstring.js
+++ src/lib/__tests__/getrandomstring.js
@@ -1,4 +1,6 @@
 jest.dontMock('../getrandomstring');
+// Always use 'require' in jest tests to avoid Babel hoisting imports before
+// 'jest.dontMock'.
 const getRandomString = require('../getrandomstring').getRandomString;
 
 describe('getRandomString', () => {
