9c32601f11fe785dc22265ff10cbc6e038a26ba1
Daniel Steigerwald <daniel@steigerwald.cz>
2015-04-11 02:58:19 +0200

    Update intl store.

--- src/client/intl/store.js
+++ src/client/intl/store.js
@@ -1,8 +1,10 @@
 import IntlMessageFormat from 'intl-messageformat';
+import IntlRelativeFormat from 'intl-relativeformat';
 import {i18nCursor} from '../state';
 import {register} from '../dispatcher';
 
 const cachedInstances = Object.create(null);
+const intlRelativeFormat = new IntlRelativeFormat;
 
 function getCachedInstanceOf(message) {
   if (message in cachedInstances)
@@ -11,11 +13,6 @@ function getCachedInstanceOf(message) {
   cachedInstances[message] = new IntlMessageFormat(message);
   return cachedInstances[message];
 }
-
-export const dispatchToken = register(({action, data}) => {
-  // TODO: Allow changing locale without app reload. Reset cache, force update
-  // root app component and PureComponents as well.
-});
 
 export function msg(path, values = null): string {
   const pathParts = ['messages'].concat(path.split('.'));
@@ -27,3 +24,17 @@ export function msg(path, values = null): string {
 
   return getCachedInstanceOf(message).format(values);
 }
+
+export function relativeDateFormat(date, options?): string {
+  return intlRelativeFormat.format(date, options);
+}
+
+export function dateFormat(date, locales?, options?): string {
+  const dateTimeFormat = new Intl.DateTimeFormat(locales, options);
+  return dateTimeFormat.format(date);
+}
+
+export const dispatchToken = register(({action, data}) => {
+  // TODO: Allow changing locale without app reload. Reset cache, force update
+  // root app component and PureComponents as well.
+});
