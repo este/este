36d4dd4e74ba0c69286ab67cf12ff45640d4da46
Daniel Steigerwald <daniel@steigerwald.cz>
2015-05-06 18:44:37 +0200

    And now it's time for reviver.
    So stores will get beloved record types.

--- src/client/state.js
+++ src/client/state.js
@@ -1,10 +1,28 @@
 import State from '../lib/state';
+import TodoItem from './todos/todoitem';
+import immutable from 'immutable';
 
 const initialState = process.env.IS_BROWSER
   ? window._appState
   : require('../server/initialstate');
 
-export const state = new State(initialState);
+// Custom revirer example, check how to convert JSON to custom record types.
+// http://facebook.github.io/immutable-js/docs/#/fromJS
+export const state = new State(initialState, function(key, value) {
+  // For the top level only.
+  if (this === initialState)
+    switch (key) {
+      case 'newTodo':
+        return new TodoItem(value.toJS());
+      case 'todos':
+        return value.map(todo => new TodoItem(todo.toJS())).toList();
+    }
+
+  return immutable.Iterable.isIndexed(value)
+    ? value.toList()
+    : value.toMap();
+});
+
 export const $pendingActionsCursor = state.cursor(['$pendingActions']);
 export const authCursor = state.cursor(['auth']);
 export const i18nCursor = state.cursor(['i18n']);
