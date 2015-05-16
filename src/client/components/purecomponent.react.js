7a44b7be504313d861b7608d9c7a31ca15be6040
Daniel Steigerwald <daniel@steigerwald.cz>
2015-05-16 03:31:04 +0200

    Update PureComponent.

--- src/client/components/purecomponent.react.js
+++ src/client/components/purecomponent.react.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import shallowEqual from 'react/lib/shallowEqual';
+import shallowEqual from 'react-pure-render/shallowEqual';
+
+// import diff from 'immutablediff'
 
 /**
- * PureRenderMixin replacement for React component ES6 classes.
- * https://github.com/facebook/react/blob/ed3e6ecb9b86b97c09428f40deb8c3ed695e73e8/src/addons/ReactComponentWithPureRenderMixin.js
+ * PureComponent is pure goodness. Always use it for two reasons:
+ *  1) The fastest possible rendering. No need to optimize anything anymore.
+ *  2) It's explicit which data aka props component needs, and we can easily
+ *     define behavior for not yet loaded data.
+ * http://facebook.github.io/react/docs/advanced-performance.html
  */
 export default class PureComponent extends React.Component {
 
+  // // Helper to check which components were changed and why.
+  // componentDidUpdate(prevProps, prevState) {
+  //   const name = this.constructor.displayName || this.constructor.name
+  //   console.log(`${name} didUpdate`)
+  //   const propsDiff = diff(prevProps, this.props).toJS()
+  //   const stateDiff = diff(prevState, this.state).toJS()
+  //   if (propsDiff.length) console.log('props', propsDiff)
+  //   if (stateDiff.length) console.log('state', stateDiff)
+  // }
+
   shouldComponentUpdate(nextProps, nextState) {
-    // https://github.com/rackt/react-router/issues/866
+    // https://github.com/gaearon/react-pure-render#known-issues
     if (this.context.router) {
       const changed = this.pureComponentLastPath !== this.context.router.getCurrentPath();
       this.pureComponentLastPath = this.context.router.getCurrentPath();
