842620a34d50f5c09947ae6008f96a1ce0716ebc
Mike Grabowski <grabbou@gmail.com>
2015-05-18 12:42:54 +0200

    Moved buttons, refactored stylesheets

--- src/client/pages/todos.react.js
+++ src/client/pages/todos.react.js
@@ -1,15 +1,12 @@
-import Buttons from './buttons.react';
-import DocumentTitle from 'react-document-title';
-import List from '../todos/list.react';
-import NewTodo from '../todos/newtodo.react';
-import PureComponent from '../components/purecomponent.react';
 import React from 'react';
-import ToCheck from './tocheck.react';
 import immutable from 'immutable';
+import DocumentTitle from 'react-document-title';
+import PureComponent from '../components/purecomponent.react';
 import {msg} from '../intl/store';
-
-// Leverage webpack require goodness.
-require('./todos.styl');
+import List from '../todos/list.react';
+import Buttons from '../todos/buttons.react';
+import NewTodo from '../todos/newtodo.react';
+import ToCheck from './tocheck.react';
 
 class Todos extends PureComponent {
 
