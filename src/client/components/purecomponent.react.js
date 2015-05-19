7be487562dd511499d6828b94f78d0d0ad46adcb
Daniel Steigerwald <daniel@steigerwald.cz>
2015-05-19 04:39:48 +0200

    Improve PureComponent "what have been changed and why" helper.

--- src/client/components/purecomponent.react.js
+++ src/client/components/purecomponent.react.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import shallowEqual from 'react-pure-render/shallowEqual';
 
-// import diff from 'immutablediff'
+// import diff from 'immutablediff';
 
 /**
  * PureComponent is pure goodness. Always use it for two reasons:
@@ -12,27 +12,35 @@ import shallowEqual from 'react-pure-render/shallowEqual';
  */
 export default class PureComponent extends React.Component {
 
-  // // Helper to check which components were changed and why.
-  // componentDidUpdate(prevProps, prevState) {
-  //   const name = this.constructor.displayName || this.constructor.name
-  //   console.log(`${name} didUpdate`)
-  //   const propsDiff = diff(prevProps, this.props).toJS()
-  //   const stateDiff = diff(prevState, this.state).toJS()
-  //   if (propsDiff.length) console.log('props', propsDiff)
-  //   if (stateDiff.length) console.log('state', stateDiff)
-  // }
-
   shouldComponentUpdate(nextProps, nextState) {
+    // TODO: Make whole React Pure, add something like dangerouslySetLocalState.
     // https://github.com/gaearon/react-pure-render#known-issues
+    // https://twitter.com/steida/status/600395820295450624
     if (this.context.router) {
       const changed = this.pureComponentLastPath !== this.context.router.getCurrentPath();
       this.pureComponentLastPath = this.context.router.getCurrentPath();
       if (changed) return true;
     }
 
-    return !shallowEqual(this.props, nextProps) ||
-           !shallowEqual(this.state, nextState);
+    const shouldUpdate =
+      !shallowEqual(this.props, nextProps) ||
+      !shallowEqual(this.state, nextState);
+
+    // if (shouldUpdate)
+    //   this._logShouldUpdateComponents(nextProps, nextState)
+
+    return shouldUpdate;
   }
+
+  // // Helper to check which component was changed and why.
+  // _logShouldUpdateComponents(nextProps, nextState) {
+  //   const name = this.constructor.displayName || this.constructor.name
+  //   console.log(`${name} shouldUpdate`)
+  //   const propsDiff = diff(this.props, nextProps).toJS()
+  //   const stateDiff = diff(this.state, nextState).toJS()
+  //   if (propsDiff.length) console.log('props', propsDiff)
+  //   if (stateDiff.length) console.log('state', stateDiff)
+  // }
 
 }
 
