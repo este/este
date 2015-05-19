27d7bdb8ba13b7bd901e7568c9278daca12a2be9
Mike Grabowski <grabbou@gmail.com>
2015-05-19 13:02:42 +0200

    Initial commit -> first part of refactor done

---
+++ src/server/frontend/index.js
@@ -0,0 +1,30 @@
+import compression from 'compression';
+import config from '../config';
+import express from 'express';
+// import favicon from 'serve-favicon';
+import render from './render';
+
+const app = express();
+
+app.use(compression());
+// TODO: Add favicon.
+// app.use(favicon('assets/img/favicon.ico'))
+// TODO: Move to CDN.
+app.use('/build', express.static('build'));
+app.use('/assets', express.static('assets'));
+
+app.get('*', (req, res) => {
+  const acceptsLanguages = req.acceptsLanguages(config.appLocales);
+  render(req, res, acceptsLanguages || config.defaultLocale)
+    .catch((error) => {
+      const msg = error.stack || error;
+      console.log(msg);
+      res.status(500).send('500: ' + msg);
+    });
+});
+
+app.on('mount', () => {
+  console.log('Client app listening at %s', app.mountpath);
+});
+
+export default app;
--- src/server/frontend/render.js
+++ src/server/frontend/render.js
@@ -6,8 +6,15 @@ import Immutable from 'immutable';
 import Promise from 'bluebird';
 import React from 'react';
 import Router from 'react-router';
+<<<<<<< HEAD:src/server/render.js
 import routes from '../client/routes';
 import {state} from '../client/state';
+=======
+import config from '../config';
+import initialState from '../initialstate';
+import routes from '../../client/routes';
+import {state} from '../../client/state';
+>>>>>>> Initial commit -> first part of refactor done:src/server/frontend/render.js
 
 export default function render(req, res, preloadedState) {
   const url = req.originalUrl;
