2541521b4e17c72e296136466e2be617d959d108
Mike Grabowski <grabbou@gmail.com>
2015-05-27 08:15:59 +0200

    Initial commit

--- src/server/frontend/index.js
+++ src/server/frontend/index.js
@@ -1,6 +1,7 @@
 import compression from 'compression';
 import express from 'express';
 // import favicon from 'serve-favicon';
+import config from '../config';
 import render from './render';
 
 const app = express();
@@ -12,8 +13,27 @@ app.use(compression());
 app.use('/build', express.static('build'));
 app.use('/assets', express.static('assets'));
 
+// Example how initialState, which is the same for all users, is enriched with
+// user state. With state-less Flux, we don't need instances.
+app.use(function(req, res, next) {
+
+  const acceptsLanguages = req.acceptsLanguages(config.appLocales);
+
+  req.userState = {
+    i18n: {
+      locales: acceptsLanguages || config.defaultLocale
+    }
+  };
+
+  // Simulate async loading from DB.
+  setTimeout(() => {
+    next();
+  }, 20);
+
+});
+
 app.get('*', (req, res, next) => {
-  render(req, res).catch(next);
+  render(req, res, req.userState).catch(next);
 });
 
 app.on('mount', () => {
