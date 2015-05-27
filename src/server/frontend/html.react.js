1cfc5eb81184a2c1eee49695bfe1b69ea34e4d60
Daniel Steigerwald <daniel@steigerwald.cz>
2015-05-27 02:46:14 +0200

    Close #227

--- src/server/frontend/html.react.js
+++ src/server/frontend/html.react.js
@@ -11,12 +11,6 @@ export default class Html extends Component {
         rel="stylesheet"
       />;
 
-    // TODO: Add favicon.
-    const linkFavicon = false && <link
-      href={`/build/img/favicon.icon?v=${this.props.version}`}
-      rel="shortcut icon"
-    />;
-
     return (
       <html lang="en">
         <head>
@@ -24,7 +18,6 @@ export default class Html extends Component {
           <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
           <title>{this.props.title}</title>
           {linkStyles}
-          {linkFavicon}
         </head>
         <body dangerouslySetInnerHTML={{__html: this.props.bodyHtml}} />
       </html>
