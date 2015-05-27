d43e9beeb674554af971df0e5ff1b700617f02fc
Samuel Hapák <samuel.hapak@gmail.com>
2015-05-27 17:05:40 +0200

    Fix semi-colons.

--- src/lib/atom.js
+++ src/lib/atom.js
@@ -1,21 +1,21 @@
-import EventEmitter from 'eventemitter3'
+import EventEmitter from 'eventemitter3';
 
 export default class Atom extends EventEmitter {
 
   constructor(value) {
-    super()
-    this._value = value
+    super();
+    this._value = value;
   }
 
   set(value) {
-    if (this._value === value) return
-    const oldValue = this._value
-    this._value = value
-    this.emit('change', value, oldValue)
+    if (this._value === value) return;
+    const oldValue = this._value;
+    this._value = value;
+    this.emit('change', value, oldValue);
   }
 
   get() {
-    return this._value
+    return this._value;
   }
 
   toString() {return "Atom(" + this._value + ")";}
--- src/lib/cursor.js
+++ src/lib/cursor.js
@@ -1,16 +1,16 @@
 export default class Cursor {
 
   constructor(ref, path: Array<string>) {
-    this._ref = ref
-    this._path = path
+    this._ref = ref;
+    this._path = path;
   }
 
   set(value) {
-    this._ref.set(this._ref.get().setIn(this._path, value))
+    this._ref.set(this._ref.get().setIn(this._path, value));
   }
 
   get() {
-    return this._ref.get().getIn(this._path)
+    return this._ref.get().getIn(this._path);
   }
 
   toString() {return "Cursor(" + this._ref + ", " + this._path + ")";}
