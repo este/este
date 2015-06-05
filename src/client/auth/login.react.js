7ccc95bc7409ec15d11d17847fc5627646220048
Daniel Steigerwald <daniel@steigerwald.cz>
2015-06-05 01:42:32 -0700

    Close #259

--- src/client/auth/login.react.js
+++ src/client/auth/login.react.js
@@ -1,6 +1,5 @@
 import * as actions from './actions';
 import Component from '../components/component.react';
-import DocumentTitle from 'react-document-title';
 import React from 'react';
 import exposeRouter from '../components/exposerouter.react';
 import immutable from 'immutable';
@@ -34,43 +33,41 @@ class Login extends Component {
     const form = this.getForm();
 
     return (
-      <DocumentTitle title={msg('auth.title')}>
-        <div className="login">
-          <form onSubmit={(e) => this.onFormSubmit(e)}>
-            <fieldset disabled={actions.login.pending}>
-              <legend>{msg('auth.form.legend')}</legend>
-              <input
-                autoFocus
-                name="email"
-                onChange={actions.updateFormField}
-                placeholder={msg('auth.form.placeholder.email')}
-                value={form.fields.email}
-              />
-              <br />
-              <input
-                name="password"
-                onChange={actions.updateFormField}
-                placeholder={msg('auth.form.placeholder.password')}
-                type="password"
-                value={form.fields.password}
-              />
-              <br />
-              <button
-                children={msg('auth.form.button.login')}
-                disabled={actions.login.pending}
-                type="submit"
-              />
-              {/*
-               <button type="submit">{msg('auth.form.button.signup')}</button>
-              */}
-              {form.error &&
-                <span className="error-message">{form.error.message}</span>
-              }
-              <div>{msg('auth.form.hint')}</div>
-            </fieldset>
-          </form>
-        </div>
-      </DocumentTitle>
+      <div className="login">
+        <form onSubmit={(e) => this.onFormSubmit(e)}>
+          <fieldset disabled={actions.login.pending}>
+            <legend>{msg('auth.form.legend')}</legend>
+            <input
+              autoFocus
+              name="email"
+              onChange={actions.updateFormField}
+              placeholder={msg('auth.form.placeholder.email')}
+              value={form.fields.email}
+            />
+            <br />
+            <input
+              name="password"
+              onChange={actions.updateFormField}
+              placeholder={msg('auth.form.placeholder.password')}
+              type="password"
+              value={form.fields.password}
+            />
+            <br />
+            <button
+              children={msg('auth.form.button.login')}
+              disabled={actions.login.pending}
+              type="submit"
+            />
+            {/*
+             <button type="submit">{msg('auth.form.button.signup')}</button>
+            */}
+            {form.error &&
+              <span className="error-message">{form.error.message}</span>
+            }
+            <div>{msg('auth.form.hint')}</div>
+          </fieldset>
+        </form>
+      </div>
     );
   }
 
