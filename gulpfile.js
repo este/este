c8d9c09579a9d32054de9b4ecb1cc1ece2a91437
Eduard Zintz <e@zintz.cc>
2015-06-13 18:57:55 +0200

    Fixed missing semicolon

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ var args = yargs
 
 gulp.task('env', function() {
   process.env.NODE_ENV = args.production ? 'production' : 'development';
-  process.env.CONTINUOUS_INTEGRATION = !!process.env.CIRCLECI
+  process.env.CONTINUOUS_INTEGRATION = !!process.env.CIRCLECI;
 });
 
 gulp.task('build-webpack-production', webpackBuild(makeWebpackConfig(false)));
