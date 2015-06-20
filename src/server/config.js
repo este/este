3235e1e88f357be9172b45edf32a276065b7c291
Daniel Steigerwald <daniel@steigerwald.cz>
2015-06-19 23:46:37 -0700

    Update comment about piping hook.

--- src/server/config.js
+++ src/server/config.js
@@ -12,8 +12,7 @@ var config = {
   piping: {
     // Ignore webpack custom loaders on server. TODO: Reuse index.js config.
     ignore: /(\/\.|~$|\.(css|less|sass|scss|styl))/,
-    // Hook ensures server restart on all required deps, even client side.
-    // Server restarting invalidates require cache, no more stale html.
+    // Hook ensures always fresh server response even for client file change.
     hook: true
   },
   port: process.env.PORT || 8000,
