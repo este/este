2ef5b79e125333ebfffea3ab8a89b5d5e7fe89b1
Daniel Steigerwald <daniel@steigerwald.cz>
2015-06-22 04:01:28 -0700

    Add dumb test example.

---
+++ src/test/client/todos/actions.spec.js
@@ -0,0 +1,9 @@
+import * as actions from 'client/todos/actions';
+import {expect} from 'chai';
+
+describe('addTodo', () => {
+  it('is a function', () => {
+    // TODO: Add real test.
+    expect(actions.addTodo).to.be.a('function');
+  });
+});
