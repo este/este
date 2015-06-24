98d6d3313bb43fac9fcda1e3cc6c5e37b33c2ef1
Lukáš Voda <xeedus@gmail.com>
2015-06-24 15:23:43 +0200

    Display formatted error page when error occurs during server rendering

---
+++ src/server/lib/errorhandler.js
@@ -0,0 +1,34 @@
+import config from '../config';
+
+// Four arguments need to be defined in order for the middleware to act
+// as an error handler.
+export default (err, req, res, next) => {
+  const errorDetails = err.stack || err;
+
+  console.error('Yay', errorDetails);
+
+  res.status(500).format({
+    json() {
+      const errorInfo = {error: err.toString()};
+      if (!config.isProduction) errorInfo.details = errorDetails;
+
+      res.send(errorInfo);
+    },
+
+    html() {
+      const message = config.isProduction
+        ? '<p>Something went wrong</p>'
+        : `<pre>${errorDetails}</pre>`;
+
+      res.send(`<h1>500 Internal server error</h1>\n${message}`);
+    },
+
+    default() {
+      const message = config.isProduction
+        ? 'Something went wrong'
+        : `${errorDetails}`;
+
+      res.send(`500 Internal server error:\n${message}`);
+    }
+  });
+};
