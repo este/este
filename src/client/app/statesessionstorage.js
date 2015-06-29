ca9f4648a39ebf88b10987bcef4b48b645ab1522
Lukáš Voda <xeedus@gmail.com>
2015-06-29 17:38:37 +0200

    Add persistence to sessionStorage for appState

---
+++ src/client/app/statesessionstorage.js
@@ -0,0 +1,17 @@
+export function listenStateChange(state) {
+  state.on('change', () =>
+    window.sessionStorage.setItem('appState', JSON.stringify(state.save()))
+  );
+}
+
+export function getPersistedState() {
+  const stateStr = window.sessionStorage.getItem('appState');
+  if (!stateStr) return null;
+
+  try {
+    return JSON.parse(stateStr);
+  } catch(error) {
+    console.error('Error while deserializing state from sessionStorage', error); // eslint-disable-line no-console
+    return null;
+  }
+}
