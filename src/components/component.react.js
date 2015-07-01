16c9a1ab9ffcd5100cd444194090fb341176e81d
Mike Grabowski <grabbou@gmail.com>
2015-07-01 10:44:25 +0200

    Final input

--- src/components/component.react.js
+++ src/components/component.react.js
@@ -8,18 +8,7 @@ import shallowEqual from 'react-pure-render/shallowEqual';
 class Component extends React.Component {
 
   shouldComponentUpdate(nextProps, nextState) {
-
-    if (this.context.router) {
-      const changed = this.pureComponentLastPath !== this.context.router.getCurrentPath();
-      this.pureComponentLastPath = this.context.router.getCurrentPath();
-      if (changed) return true;
-    }
-
-    const shouldUpdate =
-      !shallowEqual(this.props, nextProps) ||
-      !shallowEqual(this.state, nextState);
-
-    return shouldUpdate;
+    return !shallowEqual(this.props, nextProps) || !shallowEqual(this.state, nextState);
   }
 
 }
--- src/components/input.react.js
+++ src/components/input.react.js
@@ -1,30 +1,16 @@
 import React from 'react-native';
-import Component from './component.react';
 import {
   TextInput
 } from 'react-native';
 
-class Input extends Component {
+class Input extends React.Component {
 
-  /**
-   * This is on purpose. In this particular case, we do not want
-   * to keep the input in sync with the passed value as this produces
-   * flickering due to some race conditions between Objective-C / Javascript.
-   * In order to keep it blazing fast yet correct, we sync on blur, once
-   * user finishes editing
-   */
   constructor(props) {
     super(props);
     this.onFieldChange = this.onFieldChange.bind(this);
     this.onEndEditing = this.onEndEditing.bind(this);
-    this.state = {
-      value: this.props.value
-    };
   }
 
-  /**
-   * Bubbles up native event
-   */
   onFieldChange(event) {
     this.props.onChange({
       target: {
@@ -32,50 +18,28 @@ class Input extends Component {
         value: event.nativeEvent.text
       }
     });
-    this.setState({
-      editing: true
-    });
   }
 
-  /**
-   * When editing finishes, we update the in-component state
-   * with the current value passed via props.
-   */
   onEndEditing() {
-    this.props.onEndEditing();
-    this.setState({
-      value: this.props.value,
-      editing: true
+    this.refs.textInput.setNativeProps({
+      text: this.props.value
     });
+    this.props.onEndEditing();
   }
 
-  /**
-   * When input is not focused, update its state
-   * This cannot happen while user is entering text to avoid
-   * race conditions hence the check
-   */
   componentWillReceiveProps({value}) {
-    if (!this.state.editing)
-      this.setState({
+    if (!value.length)
+      this.refs.textInput.setNativeProps({
         value: value
       });
   }
 
-  /**
-   * Dismisses the underlaying textInput
-   * making the keyboard dissapear. This is just a proxy to NativeMixins
-   * that are only exposed on built-in React Native components (like TextInput)
-   */
   blur() {
     this.refs.textInput.blur();
   }
 
-  /**
-   * Renders actual input
-   */
   render() {
-    const value = this.state.value;
-
+    const value = this.props.value;
     return (
       <TextInput
         {...this.props}
