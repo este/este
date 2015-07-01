23be362b4f41ea7fcb075b905ea579fd7260b5c4
Mike Grabowski <grabbou@gmail.com>
2015-07-01 13:36:35 +0200

    Add boilerplate for header

--- src/pages/home.react.js
+++ src/pages/home.react.js
@@ -1,5 +1,6 @@
 import Component from '../components/component.react';
 import React from 'react-native';
+import TodoHeader from '../todos/todoheader.react';
 import {
   Text,
   View
@@ -10,13 +11,21 @@ import style from './todos.style';
 class Home extends Component {
 
   render() {
+    const {navigation} = this.props;
     return (
       <View style={style.container}>
+        <TodoHeader
+          navigation={navigation}
+        />
         <Text>Wat</Text>
       </View>
     );
   }
 
 }
+
+Home.propTypes = {
+  navigation: React.PropTypes.object.isRequired
+};
 
 export default Home;
