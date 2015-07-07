3d5a81722eda072a0a3b263a4227b0fe10aef188
Lukas Jecha <lukas.jecha@avg.com>
2015-07-07 15:58:28 +0200

    #311 move tests into proper folders in src/client | server

--- src/test/index.js
+++ src/test/index.js
@@ -1,5 +1,5 @@
 // Browser ES6 Polyfill
 require('babel/polyfill');
 
-var testsContext = require.context('.', true, /spec.js$/);
+var testsContext = require.context('../', true, /spec.js$/);
 testsContext.keys().forEach(testsContext);
