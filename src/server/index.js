49d93c51b42214e4ac7160f4fb9e2b6e3f03a614
Daniel Steigerwald <daniel@steigerwald.cz>
2015-07-08 12:13:04 +0200

    Improve error message for npm start without NODE_ENV.

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ const config = require('./config');
 
 if (config.isProduction || require('piping')(config.piping)) {
   if (!process.env.NODE_ENV)
-    throw new Error('Environment variable NODE_ENV must be set.');
+    throw new Error('Environment variable NODE_ENV isn\'t set. Remember it\'s up your production enviroment to set NODE_ENV and maybe other variables. To run app locally in production mode, use gulp -p command instead.');
 
   // Load and use polyfill for ECMA-402.
   if (!global.Intl)
