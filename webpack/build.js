0677b10bd5456112a0f2425229f967fa158f76f9
Daniel Steigerwald <daniel@steigerwald.cz>
2015-07-12 20:54:09 +0200

    Add hint how to analyse webpack bundle.

--- webpack/build.js
+++ webpack/build.js
@@ -9,6 +9,13 @@ module.exports = function(webpackConfig) {
   return function(callback) {
     webpack(webpackConfig, function(fatalError, stats) {
       var jsonStats = stats.toJson();
+
+      // We can save jsonStats to be analyzed with
+      // http://webpack.github.io/analyse or
+      // https://github.com/robertknight/webpack-bundle-size-analyzer.
+      // var fs = require('fs');
+      // fs.writeFileSync('./bundle-stats.json', JSON.stringify(jsonStats));
+
       var buildError = fatalError || jsonStats.errors[0] || jsonStats.warnings[0];
 
       if (buildError)
