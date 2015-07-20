02f3268045c8081ec2bd85a67b18bfbaa30259dd
Daniel Steigerwald <daniel@steigerwald.cz>
2015-07-20 20:16:43 +0200

    Move props where they belong.

--- src/server/frontend/html.react.js
+++ src/server/frontend/html.react.js
@@ -11,10 +11,12 @@ export default class Html extends Component {
   };
 
   render() {
+    const {bodyHtml, isProduction, title, version} = this.props;
+
     // Only for production. For dev, it's handled by webpack with livereload.
-    const linkStyles = this.props.isProduction &&
+    const linkStyles = isProduction &&
       <link
-        href={`/build/app.css?v=${this.props.version}`}
+        href={`/build/app.css?v=${version}`}
         rel="stylesheet"
       />;
 
@@ -23,10 +25,10 @@ export default class Html extends Component {
         <head>
           <meta charSet="utf-8" />
           <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
-          <title>{this.props.title}</title>
+          <title>{title}</title>
           {linkStyles}
         </head>
-        <body dangerouslySetInnerHTML={{__html: this.props.bodyHtml}} />
+        <body dangerouslySetInnerHTML={{__html: bodyHtml}} />
       </html>
     );
   }
