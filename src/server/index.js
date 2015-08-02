0a4cace800ce75ec0a75f2978a0cfab56bd33a5a
Daniel Steigerwald <daniel@steigerwald.cz>
2015-08-02 15:19:16 +0200

    Add server side language polyfill.

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,23 @@
+const areIntlLocalesSupported = require('intl-locales-supported');
 const config = require('./config');
 
 if (config.isProduction || require('piping')(config.piping)) {
   if (!process.env.NODE_ENV)
     throw new Error('Environment variable NODE_ENV isn\'t set. Remember it\'s up your production enviroment to set NODE_ENV and maybe other variables. To run app locally in production mode, use gulp -p command instead.');
 
-  // Load and use polyfill for ECMA-402.
-  if (!global.Intl)
+  if (global.Intl) {
+    // Determine if the built-in `Intl` has the locale data we need.
+    if (!areIntlLocalesSupported(config.appLocales)) {
+      // `Intl` exists, but it doesn't have the data we need, so load the
+      // polyfill and replace the constructors we need with the polyfill's.
+      require('intl');
+      global.Intl.NumberFormat = global.IntlPolyfill.NumberFormat;
+      global.Intl.DateTimeFormat = global.IntlPolyfill.DateTimeFormat;
+    }
+  } else {
+    // No `Intl`, so use and load the polyfill.
     global.Intl = require('intl');
+  }
 
   require('babel/register')({optional: ['es7']});
 
