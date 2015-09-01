badbec6e0aba939be3606359c0be7dc48c156e79
Mike Grabowski <grabbou@gmail.com>
2015-09-01 10:37:38 +0200

    React 0.10, no hacks, pure babel, upgrade your watchman

--- src/config.js
+++ src/config.js
@@ -1,11 +1,6 @@
 /* global __DEV__ */
 
 const prodConfig = {
-  api: 'http://pfc-api.herokuapp.com/v1/client',
-  stripe: {
-    url: 'https://api.stripe.com/v1',
-    token: 'pk_test_1GnxFHMHyaLGh4fcByyrpsLG'
-  },
   env: 'production'
 };
 
