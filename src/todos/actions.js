6139e3e4c4d1e332d807b0a709ba207596731c79
Mike Grabowski <grabbou@gmail.com>
2015-09-01 10:52:32 +0200

    Rewrite actions

--- src/todos/actions.js
+++ src/todos/actions.js
@@ -1,68 +1,65 @@
-import setToString from '../lib/settostring';
-import {dispatch} from '../dispatcher';
+export const actions = create();
+export const feature = 'todos';
 
-export const MAX_TODO_TITLE_LENGTH = 42;
+const MAX_TODO_TITLE_LENGTH = 42;
 
-export function addHundredTodos() {
-  dispatch(addHundredTodos);
+export function create(dispatch) {
+
+  return {
+
+    addHundredTodos() {
+      dispatch(actions.addHundredTodos);
+    },
+
+    addTodo(todo) {
+      const title = todo.title.trim();
+      if (!title) return;
+      dispatch(actions.addTodo, todo);
+    },
+
+    clearAll() {
+      dispatch(actions.clearAll);
+    },
+
+    deleteTodo(todo) {
+      dispatch(actions.deleteTodo, todo);
+    },
+
+    completeTodo(todo) {
+      dispatch(actions.completeTodo, todo);
+    },
+
+    onTodoFieldChange({id}, {target: {name, value}}) {
+      switch (name) {
+        case 'title':
+          value = value.slice(0, MAX_TODO_TITLE_LENGTH);
+          break;
+      }
+      dispatch(actions.onTodoFieldChange, {id, name, value});
+    },
+
+    onTodoEndEditing(todo) {
+      if (todo.title.length === 0)
+        dispatch(actions.deleteTodo, todo);
+    },
+
+    toggleTodoCompleted(todo) {
+      dispatch(actions.toggleTodoCompleted, todo);
+    },
+
+    clearCompletedTodos() {
+      dispatch(actions.clearCompletedTodos);
+    },
+
+    onNewTodoFieldChange({target: {name, value}}) {
+      switch (name) {
+        case 'title':
+          value = value.slice(0, MAX_TODO_TITLE_LENGTH);
+          break;
+      }
+      dispatch(actions.onNewTodoFieldChange, {name, value});
+    }
+
+  };
+
 }
-
-export function addTodo(todo) {
-  const title = todo.title.trim();
-  if (!title) return;
-  dispatch(addTodo, todo);
-}
-
-export function clearAll() {
-  dispatch(clearAll);
-}
-
-export function deleteTodo(todo) {
-  dispatch(deleteTodo, todo);
-}
-
-export function completeTodo(todo) {
-  dispatch(completeTodo, todo);
-}
-
-export function onTodoFieldChange({id}, {target: {name, value}}) {
-  switch (name) {
-    case 'title':
-      value = value.slice(0, MAX_TODO_TITLE_LENGTH);
-      break;
-  }
-  dispatch(onTodoFieldChange, {id, name, value});
-}
-
-export function onTodoEndEditing(todo) {
-  if (todo.title.length === 0)
-    dispatch(deleteTodo, todo);
-}
-
-export function toggleTodoCompleted(todo) {
-  dispatch(toggleTodoCompleted, todo);
-}
-
-export function clearCompletedTodos() {
-  dispatch(clearCompletedTodos);
-}
-
-export function onNewTodoFieldChange({target: {name, value}}) {
-  switch (name) {
-    case 'title':
-      value = value.slice(0, MAX_TODO_TITLE_LENGTH);
-      break;
-  }
-  dispatch(onNewTodoFieldChange, {name, value});
-}
-
-setToString('todos', {
-  addHundredTodos,
-  addTodo,
-  clearAll,
-  clearCompletedTodos,
-  deleteTodo,
-  onTodoFieldChange,
-  onNewTodoFieldChange,
-  toggleTodoCompleted
-});
