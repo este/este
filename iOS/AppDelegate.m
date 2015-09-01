43060ae64463008f80b8a2337b7a2e0c983a01f7
Mike Grabowski <grabbou@gmail.com>
2015-09-01 10:43:26 +0200

    Remove obsolete files, improve initial state and prepare for new flux

--- iOS/AppDelegate.m
+++ iOS/AppDelegate.m
@@ -17,20 +17,41 @@
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
-//  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  //  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"este"
                                                    launchOptions:launchOptions];
 
-  // Pass your initial state to the topmost view component
-  rootView.initialProperties = @{};
+  // Your initial state, data always populated for new users
+  // when no state is saved
+  NSDictionary *initialState = @{
+    @"i18n": @{
+      @"locales": @"en"
+    },
+    @"todos": @{
+      @"list": @[
+        @{@"id": @1, @"title": @"Buy a cat"},
+        @{@"id": @2, @"title": @"Buy another cat"}
+      ]
+    }
+  };
+  
+  // Let's load user defaults we saved previously
+  NSUserDefaults *userSettings = [NSUserDefaults standardUserDefaults];
+  
+  // Use user saved state if there is one, load initial state otherwise to give seamless
+  // user experience or just to have nice development
+  // Remember - you need to rerun app from XCode in order for these changes to propagate
+  // Normal refreshing will have no effect
+  rootView.initialProperties = [userSettings dictionaryForKey:@"state"] ?: initialState;
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [[UIViewController alloc] init];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
   return YES;
 }
 
