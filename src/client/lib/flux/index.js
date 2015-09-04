009d2ee9a83ad58bb652d08d6d70e740fc6facbb
Daniel Steigerwald <daniel@steigerwald.cz>
2015-09-05 00:44:37 +0200

    Ups.

--- src/client/lib/flux/index.js
+++ src/client/lib/flux/index.js
@@ -1,8 +1,8 @@
-import Flux from './flux.react';
+import flux from './flux.react';
 import devTools from './devtools';
 
 export default function(store) {
   return process.env.IS_BROWSER
-    ? Component => Flux(store)(devTools(Component))
-    : Flux(store);
+    ? Component => flux(store)(devTools(Component))
+    : flux(store);
 }
