b344d83aff36095a01b1acdea155bd2f5d021186
Mike Grabowski <grabbou@gmail.com>
2015-09-16 11:43:01 +0200

    Clarify docs

--- src/store.js
+++ src/store.js
@@ -6,6 +6,11 @@ import createDependencies from './lib/injectDependencies';
 // Top level reducer
 import rootReducer from './reducers';
 
+// Dependencies middleware detects all actionsCreators that return function
+// instead of a Flux Standard Action. In that case, that function is given
+// the object of registered dependencies + getState,
+// so it can destruct it and use services it needs to fullfill requests
+// See auth/actions.js for an example
 const dependenciesMiddleware = createDependencies({
   fetch: window.fetch
 });
@@ -14,4 +19,7 @@ const finalStore = compose(
   applyMiddleware(dependenciesMiddleware, promiseMiddleware)
 )(createStore);
 
+// Store can now be imported from any file,
+// e.g. import store from '../store';
+// const isLoggedIn = !!store.getState().user.token;
 export default finalStore(rootReducer);
