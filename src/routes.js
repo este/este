d344088f1e8afef92b54a519cadfc560b4dd43e2
Mike Grabowski <grabbou@gmail.com>
2015-09-16 11:45:13 +0200

    Document routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 import Todos from './pages/todos.react';
 import Home from './pages/home.react';
 
+/**
+ * Simple definition of your routes
+ * Each element must define a component
+ * Optionally it can provide style to apply to sceneView
+ * animationType to override default animation
+ * use navigation.transitionTo(key) to go to a route
+ * Feel free to extend this object with properties you need
+ */
 export const routes = {
   todos: {
-    component: Todos,
-    hideNavbar: true
+    component: Todos
   },
   home: {
     component: Home
