b02276097697f596a534d0ff246b0f957ce80411
Javi <javi.aschei@gmail.com>
2015-09-21 09:04:21 -0300

    Add camelCase files.

---
+++ web/src/server/initialState.js
@@ -0,0 +1,9 @@
+// Initial state is common for all users. It's the ideal place for constants
+// like connection string etc.
+// import config from './config';
+
+export default {
+  // config: {
+  //   firebaseUrl: config.firebaseUrl
+  // }
+};
