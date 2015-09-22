a771f2f8366a17e214b965248b54dfbef24f1fcb
Mike Grabowski <grabbou@gmail.com>
2015-09-22 22:33:58 +0200

    Express() is just fine

--- web/src/server/main.js
+++ web/src/server/main.js
@@ -3,15 +3,13 @@ import config from './config';
 import errorHandler from './lib/errorHandler';
 import express from 'express';
 import frontend from './frontend';
-import {Server} from 'http';
 
 const app = express();
-const server = Server(app);
 
 app.use('/api/v1', api);
 app.use(frontend);
 app.use(errorHandler);
 
-server.listen(config.port, () => {
+app.listen(config.port, () => {
   console.log('Server started at port %s', config.port);
 });
