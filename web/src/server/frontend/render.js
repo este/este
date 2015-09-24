a85d2c22ecd2f2c540a17b893b01a20ea1367b37
Daniel Steigerwald <daniel@steigerwald.cz>
2015-09-24 03:34:32 +0200

    Refactor to prefetch server store

--- web/src/server/frontend/render.js
+++ web/src/server/frontend/render.js
@@ -1,4 +1,3 @@
-// TODO: Refactor to files.
 import DocumentTitle from 'react-document-title';
 import Html from './html.react';
 import React from 'react';
@@ -6,36 +5,26 @@ import ReactDOMServer from 'react-dom/server';
 import config from '../config';
 import createLocation from 'history/lib/createLocation';
 import createRoutes from '../../client/createRoutes';
-import initialState from '../initialState';
+import createStore from './createStore';
 import useragent from 'useragent';
+import {HOT_RELOAD_PORT} from '../../../webpack/constants';
 import {IntlProvider} from 'react-intl';
 import {Provider} from 'react-redux';
 import {RoutingContext, match} from 'react-router';
-import {configureStore} from '@este/common';
-import {fromJS} from 'immutable';
-import {HOT_RELOAD_PORT} from '../../../webpack/constants';
 
 export default function render(req, res, next) {
-  const requestInitialState = fromJS(initialState).mergeDeep({
-    device: {
-      isMobile: ['phone', 'tablet'].indexOf(req.device.type) > -1
-    }
-  });
+  createStore(req)
+    .then(store => renderPage(store, req, res, next))
+    .catch(next);
+}
 
-  const store = configureStore(requestInitialState.toJS());
-
-  // TODO: Refactor to user state without common.
-  // const {actions} = mapDispatchToProps(store.dispatch);
-  // actions.addTodo(new Todo({title: 'relax'}));
-
+function renderPage(store, req, res, next) {
   const routes = createRoutes(() => store.getState());
   const location = createLocation(req.url);
 
   match({routes, location}, (error, redirectLocation, renderProps) => {
-    // console.log(error)
 
     if (redirectLocation) {
-      console.log('redirectLocation');
       res.redirect(301, redirectLocation.pathname + redirectLocation.search);
       return;
     }
@@ -46,47 +35,45 @@ export default function render(req, res, next) {
     }
 
     if (renderProps == null) {
-      console.log('renderProps == null');
-      // TODO: Redirect to page we already have.
       res.send(404, 'Not found');
       return;
     }
 
     const ua = useragent.is(req.headers['user-agent']);
-    const html = getPageHtml(renderProps, store, req.hostname, ua);
+    const appHtml = getAppHtml(store, renderProps);
+    const clientState = store.getState();
+    const html = getPageHtml(appHtml, clientState, req.hostname, ua);
+
     res.send(html);
   });
 }
 
-// TODO: Refactor.
-function getPageHtml(renderProps, store, hostname, ua) {
-  const needIntlPolyfill = ua.safari || (ua.ie && ua.version < '11');
+function getAppHtml(store, renderProps) {
+  return ReactDOMServer.renderToString(
+    <Provider store={store}>
+      <IntlProvider>
+        <RoutingContext {...renderProps} />
+      </IntlProvider>
+    </Provider>
+  );
+}
 
-  const appHtml = `<div id="app">${
-    ReactDOMServer.renderToString(
-      <Provider store={store}>
-        <IntlProvider>
-          <RoutingContext {...renderProps} />
-        </IntlProvider>
-      </Provider>
-    )
-  }</div>`;
-
-  const appScriptSrc = config.isProduction
-    ? '/_assets/app.js?' + config.assetsHashes.appJs
-    : `//${hostname}:${HOT_RELOAD_PORT}/build/app.js`;
-
+function getPageHtml(appHtml, clientState, hostname, ua) {
   let scriptHtml = '';
 
+  const needIntlPolyfill = ua.safari || (ua.ie && ua.version < '11');
   if (needIntlPolyfill) {
     scriptHtml += `
     <script src="/node_modules/intl/dist/Intl.min.js"></script>
     <script src="/node_modules/intl/locale-data/jsonp/en-US.js"></script>`;
   }
 
+  const appScriptSrc = config.isProduction
+    ? '/_assets/app.js?' + config.assetsHashes.appJs
+    : `//${hostname}:${HOT_RELOAD_PORT}/build/app.js`;
   scriptHtml += `
     <script>
-      window.__INITIAL_STATE__ = ${JSON.stringify(store.getState())};
+      window.__INITIAL_STATE__ = ${JSON.stringify(clientState)};
     </script>
     <script src="${appScriptSrc}"></script>
   `;
@@ -96,7 +83,7 @@ function getPageHtml(renderProps, store, hostname, ua) {
   return '<!DOCTYPE html>' + ReactDOMServer.renderToStaticMarkup(
     <Html
       appCssHash={config.assetsHashes.appCss}
-      bodyHtml={appHtml + scriptHtml}
+      bodyHtml={`<div id="app">${appHtml}</div>` + scriptHtml}
       googleAnalyticsId={config.googleAnalyticsId}
       isProduction={config.isProduction}
       title={title}
