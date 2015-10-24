c0fb2a93b63a5eab3ae97c89d456b2d4bd323bc9
Daniel Steigerwald <daniel@steigerwald.cz>
2015-10-24 03:36:27 +0200

    No need to detect supported locales

--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,24 @@
-const areIntlLocalesSupported = require('intl-locales-supported');
-const config = require('./config');
+const serverConfig = require('./config');
 
 if (!process.env.NODE_ENV)
   throw new Error('Environment variable NODE_ENV isn\'t set. Remember it\'s up your production enviroment to set NODE_ENV and maybe other variables.');
 
+// http://formatjs.io/guides/runtime-environments/#polyfill-node
 if (global.Intl) {
-  // Determine if the built-in `Intl` has the locale data we need.
-  if (!areIntlLocalesSupported(config.appLocales)) {
-    // `Intl` exists, but it doesn't have the data we need, so load the
-    // polyfill and replace the constructors we need with the polyfill's.
-    require('intl');
-    global.Intl.NumberFormat = global.IntlPolyfill.NumberFormat;
-    global.Intl.DateTimeFormat = global.IntlPolyfill.DateTimeFormat;
-  }
+  // We don't have to check whether Node runtime supports specific language,
+  // because without special build it does support only english anyway.
+  require('intl');
+  global.Intl.NumberFormat = global.IntlPolyfill.NumberFormat;
+  global.Intl.DateTimeFormat = global.IntlPolyfill.DateTimeFormat;
 }
 else {
-  // No `Intl`, so use and load the polyfill.
   global.Intl = require('intl');
 }
 
 require('babel/register')({optional: ['es7']});
 
 // To ignore webpack custom loaders on server.
-config.webpackStylesExtensions.forEach(function(ext) {
+serverConfig.webpackStylesExtensions.forEach(function(ext) {
   require.extensions['.' + ext] = function() {};
 });
 
