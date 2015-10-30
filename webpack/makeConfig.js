538ff206996a30d1f77f3730fdf77f4050893d72
Jamie Copeland <jamie@jamiecopeland.com>
2015-10-30 10:11:10 +0000

    Dynamically getting dev server IP address in config instead of using hardcoded "localhost" string to ensure hot module replacement works on devices.

--- webpack/makeConfig.js
+++ webpack/makeConfig.js
@@ -3,6 +3,7 @@ import autoprefixer from 'autoprefixer';
 import constants from './constants';
 import path from 'path';
 import webpack from 'webpack';
+import ip from 'ip';
 
 const devtools = process.env.CONTINUOUS_INTEGRATION
   ? 'inline-source-map'
@@ -18,6 +19,8 @@ const loaders = {
   'sass': '!sass-loader?indentedSyntax',
   'styl': '!stylus-loader'
 };
+
+const serverIp = ip.address();
 
 export default function makeConfig(isDevelopment) {
 
@@ -42,7 +45,7 @@ export default function makeConfig(isDevelopment) {
     devtool: isDevelopment ? devtools : '',
     entry: {
       app: isDevelopment ? [
-        `webpack-hot-middleware/client?path=http://localhost:${constants.HOT_RELOAD_PORT}/__webpack_hmr`,
+        `webpack-hot-middleware/client?path=http://${serverIp}:${constants.HOT_RELOAD_PORT}/__webpack_hmr`,
         path.join(constants.SRC_DIR, 'client/main.js')
       ] : [
         path.join(constants.SRC_DIR, 'client/main.js')
@@ -83,7 +86,7 @@ export default function makeConfig(isDevelopment) {
       path: constants.BUILD_DIR,
       filename: '[name].js',
       chunkFilename: '[name]-[chunkhash].js',
-      publicPath: `http://localhost:${constants.HOT_RELOAD_PORT}/build/`
+      publicPath: `http://${serverIp}:${constants.HOT_RELOAD_PORT}/build/`
     } : {
       path: constants.BUILD_DIR,
       filename: '[name].js',
