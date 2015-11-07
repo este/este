77e65928f13c267fdd1cf6daac0abf6cedd95e19
Lukáš Voda <xeedus@gmail.com>
2015-11-07 19:42:57 +0100

    Add Promise stubs to sinon and use Bluebird for them
    This adds reporting of unhandled rejections inside tests.
    Also add some examples of how to use it and improve tests.

--- src/browser/auth/__test__/Login.spec.js
+++ src/browser/auth/__test__/Login.spec.js
@@ -1,4 +1,3 @@
-import * as actions from '../../../common/auth/actions';
 import Form from '../../../common/auth/form';
 import Login from '../Login.react';
 
@@ -23,21 +22,29 @@ describe('Login component', () => {
     }
   };
 
+  const loginAction = sinon.stub().resolves({});
+  const replaceState = sinon.spy();
+
   const data = {
-    actions: actions,
-    history: {},
+    actions: {
+      login: loginAction
+    },
+    history: {
+      replaceState
+    },
     location: {},
     msg: msg,
     auth: {form: new Form()}
   };
 
-  let sandbox, loginForm, inputs, button;
+  let sandbox, loginComponent, inputs, button;
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
-    loginForm = TestUtils.renderIntoDocument(<Login {...data} />);
-    inputs = TestUtils.scryRenderedDOMComponentsWithTag(loginForm, 'input');
-    button = TestUtils.findRenderedDOMComponentWithTag(loginForm, 'button');
+    loginComponent = TestUtils.renderIntoDocument(<Login {...data} />);
+    inputs = TestUtils.scryRenderedDOMComponentsWithTag(loginComponent, 'input');
+    button = TestUtils.findRenderedDOMComponentWithTag(loginComponent, 'button');
+    form = TestUtils.findRenderedDOMComponentWithTag(loginComponent, 'form');
   });
 
   afterEach(() => {
@@ -47,5 +54,16 @@ describe('Login component', () => {
   it('should generate a login form', () => {
     expect(inputs.length).to.equal(2);
     expect(button).to.not.equal(null);
+  });
+
+  it('should fire login action and redirect', (done) => {
+    TestUtils.Simulate.submit(form);
+
+    expect(loginAction.calledOnce).to.be.true;
+    loginAction().then(() => {
+      expect(replaceState.calledOnce).to.be.true;
+      expect(replaceState.calledWithExactly(null, '/')).to.be.true;
+      done();
+    })
   });
 });
