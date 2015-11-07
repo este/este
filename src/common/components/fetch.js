63cd4f161d33bd289a3ac39b24790bebf2d13e2e
Lukáš Voda <xeedus@gmail.com>
2015-11-07 18:08:50 +0100

    Allow passing of multiple actions to @fetch

--- src/common/components/fetch.js
+++ src/common/components/fetch.js
@@ -1,7 +1,7 @@
 import Component from 'react-pure-render/component';
 import React, {PropTypes} from 'react';
 
-export default function fetch(action) {
+export default function fetch(...actions) {
 
   return Wrapped => class Fetch extends Component {
 
@@ -13,14 +13,17 @@ export default function fetch(action) {
 
     // This enables server side fetching.
     // Check src/server/frontend/render.js fetchComponentData function.
-    static fetchAction = action;
+    static fetchActions = actions;
 
     // This enables client side fetching, method is called only in browser.
     componentDidMount() {
       // Dispatch is injected by react-redux.
       // React router injects location and params for every routed component.
       const {dispatch, location, params} = this.props;
-      dispatch(action({location, params}));
+
+      actions.forEach(action =>
+        dispatch(action({location, params}))
+      );
     }
 
     // // TODO: Fetch if last location pathname has changed.
