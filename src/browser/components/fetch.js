5c31aa7d1b9c07708195ce4d632157ff57fccc41
Lukáš Voda <xeedus@gmail.com>
2015-11-08 12:54:20 +0100

    Abstract out creation of fetch component to share code between native/browser

---
+++ src/browser/components/fetch.js
@@ -0,0 +1,5 @@
+import Component from 'react-pure-render/component';
+import createFetch from '../../common/components/createFetch';
+import React from 'react';
+
+export default createFetch(React, Component);
