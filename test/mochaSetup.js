4d7a3256f7da50de20129f8f117d431314b88ef7
Lukáš Voda <xeedus@gmail.com>
2015-11-11 21:18:21 +0100

    Update chai and chai-immutable
    Changelogs for chai:
    - https://github.com/chaijs/chai/releases/tag/3.2.0
    - https://github.com/chaijs/chai/releases/tag/3.3.0
    - https://github.com/chaijs/chai/releases/tag/3.4.0
    
    Changelogs for chai-immutable:
    - https://github.com/astorije/chai-immutable/releases/tag/v1.2.0
    - https://github.com/astorije/chai-immutable/releases/tag/v1.3.0
    - https://github.com/astorije/chai-immutable/releases/tag/v1.4.0
    - https://github.com/astorije/chai-immutable/releases/tag/v1.5.0

--- test/mochaSetup.js
+++ test/mochaSetup.js
@@ -1,25 +1,21 @@
 /* global global */
 
-require('babel/register')({
-  stage: 0
-});
-
-let jsdom = require('jsdom');
+const jsdom = require('jsdom');
 
 // Setup the simplest document possible
-let doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 
 // Get the window object out of the document
-let win = doc.defaultView;
+const window = document.defaultView;
 
 // Set globals for mocha that make access to document and window feel
 //  natural in the test environment
-global.document = doc;
-global.window = win;
+global.document = document;
+global.window = window;
 
 // Take all properties of the window object and also attach it to the
 //  mocha global object
-propagateToGlobal(win);
+propagateToGlobal(window);
 
 // From mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
 function propagateToGlobal(window) {
