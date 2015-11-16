e45431195c84b87d2c95f2ef3224fe92b3cad558
Ondrej Bartas <ondrej@bartas.cz>
2015-11-16 10:43:48 +0100

    Add Eslint Plugin Import

--- webpack/constants.js
+++ webpack/constants.js
@@ -1,6 +1,7 @@
 import path from 'path';
 
-const ABSOLUTE_BASE = path.normalize(path.join(__dirname, '..'));
+export const ABSOLUTE_BASE = path.normalize(path.join(__dirname, '..'));
+
 
 const constants = Object.freeze({
   ABSOLUTE_BASE: ABSOLUTE_BASE,
@@ -9,7 +10,14 @@ const constants = Object.freeze({
   DIST_DIR: path.join(ABSOLUTE_BASE, 'dist'),
   SRC_DIR: path.join(ABSOLUTE_BASE, 'src'),
   ASSETS_DIR: path.join(ABSOLUTE_BASE, 'assets'),
-  HOT_RELOAD_PORT: process.env.HOT_RELOAD_PORT || 8080
+  HOT_RELOAD_PORT: process.env.HOT_RELOAD_PORT || 8080,
 });
+
+export const NODE_MODULES_DIR = constants.NODE_MODULES_DIR;
+export const BUILD_DIR = constants.BUILD_DIR;
+export const DIST_DIR = constants.DIST_DIR;
+export const SRC_DIR = constants.SRC_DIR;
+export const ASSETS_DIR = constants.ASSETS_DIR;
+export const HOT_RELOAD_PORT = constants.HOT_RELOAD_PORT;
 
 export default constants;
