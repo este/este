49162016aa3462cdba28dfb78f7fd8dfbe6ceaac
rkoberg <rob.koberg@gmail.com>
2015-11-18 10:28:52 -0800

    fixed assets path. Ensured build path will resolve correctly (even though it does for me)

--- src/server/frontend/index.js
+++ src/server/frontend/index.js
@@ -2,6 +2,7 @@ import compression from 'compression';
 import device from 'express-device';
 import esteMiddleware from '../lib/esteMiddleware';
 import express from 'express';
+import path from 'path';
 // import favicon from 'serve-favicon';
 import render from './render';
 
@@ -12,13 +13,13 @@ app.use(compression());
 
 // app.use(favicon('assets/img/favicon.ico'));
 
-// the project's root directory
+// the project's root directory for path resolution.
 const basePath = path.join(__dirname, '../../..');
 const assetsDir = path.join(basePath, 'assets');
 
 // Serve the static assets. We can cache them as they include hashes.
 app.use('/assets/img', express.static(path.join(assetsDir, 'img'), {maxAge: '200d'}));
-app.use('/_assets', express.static('build', {maxAge: '200d'}));
+app.use('/_assets', express.static(path.join(basePath, 'build'), {maxAge: '200d'}));
 
 // Intl.
 app.use('/node_modules/intl/dist', express.static('node_modules/intl/dist'));
