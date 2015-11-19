ed5d8b691fbdee1f1b56c1980ac002d967b04778
Robert Thurnher <me@robi42.net>
2015-11-19 13:16:26 +0100

    Satisfy ESLint import plugin

--- src/server/frontend/assets.js
+++ src/server/frontend/assets.js
@@ -1,5 +1,5 @@
+import config from '../config';
 import fs from '../../common/lib/fs';
-import {isProduction} from '../config';
 
 const DEFAULT = {js: 'app.js', css: 'app.css'};
 
@@ -7,7 +7,7 @@ const APP_JS_PATTERN = /^app\-\w+\.js$/;
 const APP_CSS_PATTERN = /^app\-\w+\.css$/;
 
 export default async function getAppAssetFilenamesAsync() { // eslint-disable-line space-before-function-paren
-  if (!isProduction) return DEFAULT;
+  if (!config.isProduction) return DEFAULT;
 
   try {
     const buildDirFiles = await fs.readdirAsync('build');
--- src/server/main.js
+++ src/server/main.js
@@ -1,14 +1,16 @@
 import api from './api';
+import config from './config';
 import errorHandler from './lib/errorHandler';
 import express from 'express';
 import frontend from './frontend';
-import {port} from './config';
 
 const app = express();
 
 app.use('/api/v1', api);
 app.use(frontend);
 app.use(errorHandler);
+
+const {port} = config;
 
 app.listen(port, () => {
   console.log('Server started at port %d', port);
