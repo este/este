a5e98e5279627b2d6507c0c2a9e50c791ad1b6de
Daniel Steigerwald <daniel@steigerwald.cz>
2015-11-27 20:51:56 +0100

    Improve stateToJS

--- src/common/lib/stateToJS.js
+++ src/common/lib/stateToJS.js
@@ -1,5 +1,10 @@
 export default function stateToJS(state) {
+  // Third party reducers can be mutable object without toJS.
+  const toJS = key => state[key].toJS
+    ? state[key].toJS()
+    : state[key];
+
   return Object.keys(state).reduce((acc, key) => {
-    return {...acc, [key]: state[key].toJS()};
+    return {...acc, [key]: toJS(key)};
   }, {});
 }
