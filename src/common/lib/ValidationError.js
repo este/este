f0a7322fdac40f55e9e88a7a48b3c0945dfea6f9
Daniel Steigerwald <daniel@steigerwald.cz>
2015-12-28 15:33:01 +0100

    Fix custom error.
    Babel 5 worked by mistake and Babel 6 doesn’t support it, so we have to
    use custom factory ‘make-error’.

--- src/common/lib/ValidationError.js
+++ src/common/lib/ValidationError.js
@@ -1,6 +1,8 @@
-// Prop associates error with form field name.
-export default class ValidationError extends Error {
+import {BaseError} from 'make-error';
 
+export default class ValidationError extends BaseError {
+
+  // prop associates error with form field name.
   constructor(message, prop) {
     super();
     this.message = message;
