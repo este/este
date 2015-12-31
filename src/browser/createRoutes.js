6e7f0f79b963338eec44209a4ead06ba55f2f612
Daniel Steigerwald <daniel@steigerwald.cz>
2015-12-31 03:43:53 +0100

    function syntax only for export

--- src/browser/createRoutes.js
+++ src/browser/createRoutes.js
@@ -11,12 +11,12 @@ import {IndexRoute, Route} from 'react-router';
 
 export default function createRoutes(getState) {
 
-  function requireAuth(nextState, replaceState) {
+  const requireAuth = (nextState, replaceState) => {
     const loggedInUser = getState().users.viewer;
     if (!loggedInUser) {
       replaceState({nextPathname: nextState.location.pathname}, '/login');
     }
-  }
+  };
 
   return (
     <Route component={App} path="/">
