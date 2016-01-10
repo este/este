1311ef1688e645953bdd2996229e8f301ae77d53
Daniel Steigerwald <daniel@steigerwald.cz>
2016-01-10 02:20:56 +0100

    Reformat frontend render
    Always use const for functions except exported functions.

--- src/server/frontend/render.js
+++ src/server/frontend/render.js
@@ -13,6 +13,72 @@ import {Provider} from 'react-redux';
 import {RoutingContext, match} from 'react-router';
 import {createMemoryHistory} from 'history';
 
+const fetchComponentDataAsync = async (dispatch, renderProps) => {
+  const {components, location, params} = renderProps;
+  const promises = components
+    .reduce((actions, component) => {
+      return actions.concat(component.fetchActions || []);
+    }, [])
+    .map(action => {
+      return dispatch(action({location, params}));
+    });
+  await Promise.all(promises);
+};
+
+const getAppHtml = (store, renderProps) => {
+  return ReactDOMServer.renderToString(
+    <Provider store={store}>
+      <IntlProvider>
+        <RoutingContext {...renderProps} />
+      </IntlProvider>
+    </Provider>
+  );
+};
+
+const getScriptHtml = (state, headers, hostname, appJsFilename) => {
+  let scriptHtml = '';
+  const ua = useragent.is(headers['user-agent']);
+  const needIntlPolyfill = ua.safari || (ua.ie && ua.version < '11');
+  if (needIntlPolyfill) {
+    scriptHtml += `
+      <script src="/node_modules/intl/dist/Intl.min.js"></script>
+      <script src="/node_modules/intl/locale-data/jsonp/en-US.js"></script>
+    `;
+  }
+  // Note how state is serialized. JSON.stringify is anti-pattern.
+  // https://github.com/yahoo/serialize-javascript#user-content-automatic-escaping-of-html-characters
+  return scriptHtml + `
+    <script>
+      window.__INITIAL_STATE__ = ${serialize(state)};
+    </script>
+    <script src="${appJsFilename}"></script>
+  `;
+};
+
+const renderPage = (store, renderProps, req) => {
+  const state = store.getState();
+  const {headers, hostname} = req;
+  const appHtml = getAppHtml(store, renderProps);
+  const helmet = Helmet.rewind();
+  const {
+    styles: {app: appCssFilename},
+    javascript: {app: appJsFilename}
+  } = webpackIsomorphicTools.assets();
+  const scriptHtml = getScriptHtml(state, headers, hostname, appJsFilename);
+  if (!config.isProduction) {
+    webpackIsomorphicTools.refresh();
+  }
+  return '<!DOCTYPE html>' + ReactDOMServer.renderToStaticMarkup(
+    <Html
+      appCssFilename={appCssFilename}
+      bodyHtml={`<div id="app">${appHtml}</div>${scriptHtml}`}
+      googleAnalyticsId={config.googleAnalyticsId}
+      helmet={helmet}
+      isProduction={config.isProduction}
+    />
+  );
+};
+
 export default function render(req, res, next) {
   const initialState = {
     device: {
@@ -41,7 +107,7 @@ export default function render(req, res, next) {
 
     try {
       await fetchComponentDataAsync(store.dispatch, renderProps);
-      const html = await renderPageAsync(store, renderProps, req);
+      const html = renderPage(store, renderProps, req);
       // renderProps are always defined with * route.
       // https://github.com/rackt/react-router/blob/master/docs/guides/advanced/ServerRendering.md
       const status = renderProps.routes.some(route => route.path === '*')
@@ -52,74 +118,4 @@ export default function render(req, res, next) {
       next(e);
     }
   });
-}
-
-async function fetchComponentDataAsync(dispatch, {components, location, params}) {
-  const fetchActions = components.reduce((actions, component) => {
-    return actions.concat(component.fetchActions || []);
-  }, []);
-  const promises = fetchActions.map(action => dispatch(action(
-    {location, params}
-  )));
-
-  await Promise.all(promises);
-}
-
-async function renderPageAsync(store, renderProps, req) {
-  const clientState = store.getState();
-  const {headers, hostname} = req;
-  const appHtml = getAppHtml(store, renderProps);
-  const helmet = Helmet.rewind();
-  const {
-    styles: {app: appCssFilename},
-    javascript: {app: appJsFilename}
-  } = webpackIsomorphicTools.assets();
-
-  const scriptHtml = getScriptHtml(clientState, headers, hostname, appJsFilename);
-
-  if (!config.isProduction) {
-    webpackIsomorphicTools.refresh();
-  }
-
-  return '<!DOCTYPE html>' + ReactDOMServer.renderToStaticMarkup(
-    <Html
-      appCssFilename={appCssFilename}
-      bodyHtml={`<div id="app">${appHtml}</div>${scriptHtml}`}
-      googleAnalyticsId={config.googleAnalyticsId}
-      helmet={helmet}
-      isProduction={config.isProduction}
-    />
-  );
-}
-
-function getAppHtml(store, renderProps) {
-  return ReactDOMServer.renderToString(
-    <Provider store={store}>
-      <IntlProvider>
-        <RoutingContext {...renderProps} />
-      </IntlProvider>
-    </Provider>
-  );
-}
-
-function getScriptHtml(clientState, headers, hostname, appJsFilename) {
-  let scriptHtml = '';
-
-  const ua = useragent.is(headers['user-agent']);
-  const needIntlPolyfill = ua.safari || (ua.ie && ua.version < '11');
-  if (needIntlPolyfill) {
-    scriptHtml += `
-      <script src="/node_modules/intl/dist/Intl.min.js"></script>
-      <script src="/node_modules/intl/locale-data/jsonp/en-US.js"></script>
-    `;
-  }
-
-  // Note how clientState is serialized. JSON.stringify is anti-pattern.
-  // https://github.com/yahoo/serialize-javascript#user-content-automatic-escaping-of-html-characters
-  return scriptHtml + `
-    <script>
-      window.__INITIAL_STATE__ = ${serialize(clientState)};
-    </script>
-    <script src="${appJsFilename}"></script>
-  `;
 }
