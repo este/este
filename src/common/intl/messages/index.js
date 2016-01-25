60bf1d2ebc9e3ca1ee349ff3e19f8e58364cbab9
Cyril Bioley <cyril.bioley@gmail.com>
2016-01-25 15:45:35 +0100

    Fix tiny typos in src/common/intl/messages/index.js.
    See how people are reading comments? ;)

--- src/common/intl/messages/index.js
+++ src/common/intl/messages/index.js
@@ -2,7 +2,7 @@ import en from './en';
 
 // Note messages for all languages are imported all at once. That's fine for
 // almost all apps. If gzipped messages dir is bigger then say 20 kB, congrats!
-// Your app is used and usefull, so now it's time for futher optimization. Since
+// Your app is used and useful, so now it's time for further optimization. Since
 // messages are in app state, they can be fetched or passed from server easily.
 
 export default {en};
