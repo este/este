ad4b186900537a151e7d66875cc262a5eebe763b
Daniel Steigerwald <daniel@steigerwald.cz>
2016-01-27 03:39:26 +0100

    Chore

--- src/browser/todos/Todos.react.js
+++ src/browser/todos/Todos.react.js
@@ -16,6 +16,7 @@ export default class Todos extends Component {
     if (!map.size)
       return <p>{msg.empty}</p>;
 
+    // Note this is naive approach. Huge lists should be presorted in reducer.
     const list = map.toList().sortBy(item => item.createdAt);
 
     return (
