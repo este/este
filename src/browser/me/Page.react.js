ec59dabe1b7068b4ef0e51f5eee3c0d18b794831
Cyril Bioley <cyril.bioley@gmail.com>
2016-02-01 22:00:45 +0100

    Switch Stylus stylesheets to Sass

--- src/browser/me/Page.react.js
+++ src/browser/me/Page.react.js
@@ -1,4 +1,4 @@
-import './Page.styl';
+import './Page.scss';
 import AuthLogout from '../auth/Logout.react';
 import Component from 'react-pure-render/component';
 import Helmet from 'react-helmet';
