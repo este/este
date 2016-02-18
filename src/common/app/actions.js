4aa5a7809af094e140002c2f336dd09d5d751733
Robin Pokorný <me@robinpokorny.com>
2016-02-18 11:09:48 +0100

    Safer DI middleware

--- src/common/app/actions.js
+++ src/common/app/actions.js
@@ -3,19 +3,19 @@ import * as firebaseAction from '../lib/redux-firebase/actions';
 export const ON_APP_COMPONENT_DID_MOUNT = 'ON_APP_COMPONENT_DID_MOUNT';
 
 export function onAppComponentDidMount() {
-  // Who injected firebase and store? Check configureStore.js injectMiddleware.
-  return ({firebase, store}) => {
+  // Who injected firebase and dispatch? Check configureStore.js injectMiddleware.
+  return ({firebase, dispatch}) => {
 
     // Firebase has two methods to get user auth.
     //  - getAuth is sync, because it uses localStorage.
     //  - onAuth is async, because it monitors current auth from server.
 
     // Sync is nice, because it updates app state immediately.
-    store.dispatch(firebaseAction.onAuth(firebase.getAuth()));
+    dispatch(firebaseAction.onAuth(firebase.getAuth()));
 
     // Async handling of login / logout.
     firebase.onAuth(authData => {
-      store.dispatch(firebaseAction.onAuth(authData));
+      dispatch(firebaseAction.onAuth(authData));
     });
 
     return {
