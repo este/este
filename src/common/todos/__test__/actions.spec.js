b0798867d8e032ef4a036bb884da96edd5f17e12
Daniel Steigerwald <daniel@steigerwald.cz>
2016-02-20 00:20:53 +0100

    Add example test

---
+++ src/common/todos/__test__/actions.spec.js
@@ -0,0 +1,27 @@
+import * as actions from '../actions.js';
+import {expect} from 'chai';
+
+describe('todos actions', () => {
+
+  // // Async testing example.
+  // it('should do something async', done => {
+  //   setTimeout(() => {
+  //     expect('foo').equal('bar');
+  //     done();
+  //   }, 500)
+  // });
+
+  it('should create todo', () => {
+    const deps = {
+      getUid: () => 'uid',
+      now: () => 'now'
+    };
+    const action = actions.addTodo('Hello')(deps);
+    const {type, payload} = action;
+    expect(type).equal('ADD_TODO');
+    expect(payload.createdAt).equal('now');
+    expect(payload.id).equal('uid');
+    expect(payload.title).equal('Hello');
+  });
+
+});
