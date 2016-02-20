17ed17d3a52f6b779ba008f8ffff1e2c96446eb4
Daniel Steigerwald <daniel@steigerwald.cz>
2016-02-20 01:35:03 +0100

    Dispatch logout on Firebase falsy authData
    It ensures app state is recycled on user auth session timeout.

--- src/common/app/actions.js
+++ src/common/app/actions.js
@@ -1,4 +1,5 @@
-import * as firebaseAction from '../lib/redux-firebase/actions';
+import * as authActions from '../auth/actions';
+import * as firebaseActions from '../lib/redux-firebase/actions';
 
 export const ON_APP_COMPONENT_DID_MOUNT = 'ON_APP_COMPONENT_DID_MOUNT';
 
@@ -11,11 +12,12 @@ export function onAppComponentDidMount() {
     //  - onAuth is async, because it monitors current auth from server.
 
     // Sync is nice, because it updates app state immediately.
-    store.dispatch(firebaseAction.onAuth(firebase.getAuth()));
+    store.dispatch(firebaseActions.onAuth(firebase.getAuth()));
 
     // Async handling of login / logout.
     firebase.onAuth(authData => {
-      store.dispatch(firebaseAction.onAuth(authData));
+      if (!authData) store.dispatch(authActions.logout());
+      store.dispatch(firebaseActions.onAuth(authData));
     });
 
     return {
