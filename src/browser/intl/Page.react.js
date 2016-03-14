d661eb9445a81a66efb47d2d77d37243a18b229b
Daniel Steigerwald <daniel@steigerwald.cz>
2016-03-14 01:36:42 +0100

    Fix intl relative time example

--- src/browser/intl/Page.react.js
+++ src/browser/intl/Page.react.js
@@ -36,10 +36,16 @@ class Page extends Component {
     intl: intlShape.isRequired
   };
 
+  constructor(props) {
+    super(props);
+    this.componentRenderedAt = Date.now();
+  }
+
   render() {
     const { intl } = this.props;
     const title = intl.formatMessage(messages.title);
-    const unreadCount = 42007;
+    // To remember beloved −123 min. https://www.youtube.com/watch?v=VKOv1I8zKso
+    const unreadCount = 123;
 
     return (
       <div className="intl-page">
@@ -60,8 +66,8 @@ class Page extends Component {
         </p>
         <p>
           <FormattedRelative
-            value={Date.now()}
-            updateInterval={1000 * 10}
+            updateInterval={1000 * 1}
+            value={this.componentRenderedAt}
           />
         </p>
       </div>
