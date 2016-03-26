b26c07704572bc40f5db9d0a1ffcd6c798e116ae
atypicalrobot <atypicalrobot@users.noreply.github.com>
2016-03-26 18:49:54 +0000

    Add Romanian translations for #811

--- messages/ro.js
+++ messages/ro.js
@@ -69,6 +69,174 @@ export default [
     "defaultMessage": "Autentificare"
   },
   {
+    "id": "firebase.error.authenticationProviderDisabled",
+    "defaultMessage": "Furnizorul de autentificare solicitat este dezactivat pentru acest Firebase."
+  },
+  {
+    "id": "firebase.error.dataStale",
+    "defaultMessage": "Uz intern."
+  },
+  {
+    "id": "firebase.error.deniedByUser",
+    "defaultMessage": "Utilizatorul nu a autorizat aplicaţia."
+  },
+  {
+    "id": "firebase.error.disconnected",
+    "defaultMessage": "Operațiunea a trebuit să fie anulată din cauza unei deconectări de rețea."
+  },
+  {
+    "id": "firebase.error.emailTaken",
+    "defaultMessage": "Noul cont de utilizator nu poate fi creat, deoarece adresa de email specificată este deja în uz."
+  },
+  {
+    "id": "firebase.error.expiredToken",
+    "defaultMessage": "Token-ul autentificat furnizat a expirat."
+  },
+  {
+    "id": "firebase.error.invalidAuthArguments",
+    "defaultMessage": "Acreditările specificate sunt malformate sau incomplete."
+  },
+  {
+    "id": "firebase.error.invalidConfiguration",
+    "defaultMessage": "Furnizorul de autentificare solicitat nu este configurat corect, iar cererea nu poate finalizată."
+  },
+  {
+    "id": "firebase.error.invalidCredentials",
+    "defaultMessage": "Acreditările de autentificare specificate nu sunt valide."
+  },
+  {
+    "id": "firebase.error.invalidEmail",
+    "defaultMessage": "Emailul specificat nu este un email valid."
+  },
+  {
+    "id": "firebase.error.invalidPassword",
+    "defaultMessage": "Parola specificată contului de utilizator este incorectă."
+  },
+  {
+    "id": "firebase.error.invalidProvider",
+    "defaultMessage": "Furnizorul de autentificare solicitat nu există."
+  },
+  {
+    "id": "firebase.error.invalidToken",
+    "defaultMessage": "Token-ul de autentificare solicitat nu e valid."
+  },
+  {
+    "id": "firebase.error.limitsExceeded",
+    "defaultMessage": "Limite depășite."
+  },
+  {
+    "id": "firebase.error.maxRetries",
+    "defaultMessage": "Tranzacția a avut prea multe reîncercări."
+    },
+  {
+    "id": "firebase.error.networkError",
+    "defaultMessage": "Operația nu a putut fi realizată din cauza unei erori de rețea."
+  },
+  {
+    "id": "firebase.error.operationFailed",
+    "defaultMessage": "Serverul a indicat că această operațiune a eșuat."
+  },
+  {
+    "id": "firebase.error.overriddenBySet",
+    "defaultMessage": "Tranzacția a fost înlocuită de setul ulterior."
+  },
+  {
+    "id": "firebase.error.permissionDenied",
+    "defaultMessage": "Acest client nu are permisiunea de a efectua această operațiune."
+  },
+  {
+    "id": "firebase.error.preempted",
+    "defaultMessage": "Acreditarile de autentificare în așteptare sau active au fost înlocuite cu un alt apel de autentificare."
+  },
+  {
+    "id": "firebase.error.providerError",
+    "defaultMessage": "A apărut o eroare la furnizorul terț."
+  },
+  {
+    "id": "firebase.error.unavailable",
+    "defaultMessage": "Serviciul este indisponibil."
+  },
+  {
+    "id": "firebase.error.unknownError",
+    "defaultMessage": "A apărut o eroare necunoscută."
+  },
+  {
+    "id": "firebase.error.userCancelled",
+    "defaultMessage": "Utilizatorul a anulat autentificarea."
+  },
+  {
+    "id": "firebase.error.userCodeException",
+    "defaultMessage": "O excepție a avut loc în codul de utilizatorului."
+  },
+  {
+    "id": "firebase.error.userDoesNotExist",
+    "defaultMessage": "Contul de utilizator specificat nu există."
+  },
+  {
+    "id": "firebase.error.writeCanceled",
+    "defaultMessage": "Eroare la salvare in Firebase,a fost anulat pe plan local."
+  },
+  {
+    "id": "firebase.login.dismiss",
+    "defaultMessage": "Destitui"
+  },
+  {
+    "id": "firebase.login.emailLoginOrSignUp",
+    "defaultMessage": "Logare / Înregistrare Email"
+  },
+  {
+    "id": "firebase.login.emailPasswordRecovery",
+    "defaultMessage": "Recuperarea parolei de email"
+  },
+  {
+    "id": "firebase.login.emailPlaceholder",
+    "defaultMessage": "altău@email.com"
+  },
+  {
+    "id": "firebase.login.facebookLogin",
+    "defaultMessage": "Logare Facebook"
+  },
+  {
+    "id": "firebase.login.loginButton",
+    "defaultMessage": "Logare"
+  },
+  {
+    "id": "firebase.login.passwordForgotten",
+    "defaultMessage": "Ai uitat parola?"
+  },
+  {
+    "id": "firebase.login.passwordPlaceholder",
+    "defaultMessage": "Parolă"
+  },
+  {
+    "id": "firebase.login.recoveryEmailSent",
+    "defaultMessage": "Emailul de recuperare a fost trimis."
+  },
+  {
+    "id": "firebase.login.resetPassword",
+    "defaultMessage": "Resetează parola"
+  },
+  {
+    "id": "firebase.login.signUp",
+    "defaultMessage": "Înregistrare"
+  },
+  {
+    "id": "firebase.page.loadMore",
+    "defaultMessage": "Încarcă mai mult"
+  },
+  {
+    "id": "firebase.profile.greeting",
+    "defaultMessage": "Salut"
+  },
+  {
+    "id": "firebase.profile.logout",
+    "defaultMessage": "Deconectare"
+  },
+  {
+    "id": "firebase.users.lastLoggedInUsers",
+    "defaultMessage": "Ultima {limitaLaUltimul} logat in utilizatori"
+  },
+  {
     "id": "footer.madeByHtml",
     "defaultMessage": "realizat de <a href=\"https://twitter.com/steida\">steida</a>"
   },
