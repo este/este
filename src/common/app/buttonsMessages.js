6ae5b01c71a3bfff0f539189cd4dd28c803efe90
Daniel Steigerwald <daniel@steigerwald.cz>
2016-05-01 04:50:37 +0200

    Add useful common app messages

---
+++ src/common/app/buttonsMessages.js
@@ -0,0 +1,28 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  cancel: {
+    defaultMessage: 'Cancel',
+    id: 'app.buttons.cancel'
+  },
+  delete: {
+    defaultMessage: 'Delete',
+    id: 'app.buttons.delete'
+  },
+  edit: {
+    defaultMessage: 'Edit',
+    id: 'app.buttons.edit'
+  },
+  login: {
+    defaultMessage: 'Login',
+    id: 'app.buttons.login'
+  },
+  logout: {
+    defaultMessage: 'Logout',
+    id: 'app.buttons.logout'
+  },
+  save: {
+    defaultMessage: 'Save',
+    id: 'app.buttons.save'
+  }
+});
---
+++ src/common/app/confirmMessages.js
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  areYouSure: {
+    defaultMessage: 'Are you sure?',
+    id: 'app.confirm.areYouSure'
+  },
+  unsavedChanges: {
+    defaultMessage: 'You have unsaved changes. Are you sure?',
+    id: 'app.confirm.unsavedChanges'
+  }
+});
