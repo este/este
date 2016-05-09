134fde6ecc8a45ca253fe3c1ada44db9a972610f
Daniel Steigerwald <daniel@steigerwald.cz>
2016-05-09 22:46:58 +0200

    Use eval-source-map instead of cheap-module-eval-source-ma
    Until the bug will be resolved.

--- webpack/makeConfig.js
+++ webpack/makeConfig.js
@@ -12,7 +12,9 @@ const webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(webpackIso
 // cheap-module-eval-source-map, because we want original source, but we don't
 // care about columns, which makes this devtool faster than eval-source-map.
 // http://webpack.github.io/docs/configuration.html#devtool
-const devtools = 'cheap-module-eval-source-map';
+// const devtools = 'cheap-module-eval-source-map';
+// TODO: https://github.com/webpack/webpack/issues/2145
+const devtools = 'eval-source-map';
 
 const loaders = {
   css: '',
