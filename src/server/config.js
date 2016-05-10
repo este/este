e90bebefc9dd933677d52a34a220969f775391a8
Daniel Steigerwald <daniel@steigerwald.cz>
2016-05-10 05:14:34 +0200

    Ok, postpone appVersion because Heroku

--- src/server/config.js
+++ src/server/config.js
@@ -1,6 +1,6 @@
 // http://www.codedependant.net/2015/01/31/production-ready-node-configuration
 import nconf from 'nconf';
-import { execSync } from 'child_process';
+// import { execSync } from 'child_process';
 
 // Specifying an env delimiter allows us to override config when shipping to
 // production server. 'foo__bar=2 gulp' will set config to '{foo: {bar: 2}}'
@@ -11,16 +11,18 @@ nconf.env('__');
 
 const appName = require('../../package.json').name;
 
-// The semver is for libraries, apps are versioned by git commit SHA.
-const sourceVersion = process.env.SOURCE_VERSION || execSync('git rev-parse HEAD')
-  .toString()
-  .trim();
+// // The semver is for libraries, apps are versioned by git commit SHA.
+// const sourceVersion = process.env.SOURCE_VERSION || execSync('git rev-parse HEAD')
+//   .toString()
+//   .trim();
 
 // Remember, never put secrets in default config.
 // Use environment variables for production, and secrets.json for development.
 nconf.defaults({
   appName,
-  appVersion: sourceVersion.slice(0, 7),
+  // TODO: Fix sourceVersion on Heroku.
+  // appVersion: sourceVersion.slice(0, 7),
+  appVersion: 1,
   defaultLocale: 'en',
   firebaseUrl: 'https://este.firebaseio.com',
   googleAnalyticsId: 'UA-XXXXXXX-X',
