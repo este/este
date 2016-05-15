aa3e366ab8b0db4c3fdb555cbc47e954b40e1196
Daniel Steigerwald <daniel@steigerwald.cz>
2016-05-15 06:18:06 +0200

    Close #872

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,13 +1,25 @@
- // Bootstrap environment
+// Bootstrap environment
+
 const onWindowIntl = () => {
   require('babel-polyfill');
   window.Promise = require('../common/configureBluebird');
+
+  // App locales are defined in src/server/config.js
+  const { addLocaleData } = require('react-intl');
+  const cs = require('react-intl/locale-data/cs');
+  const de = require('react-intl/locale-data/de');
+  const en = require('react-intl/locale-data/en');
+  const es = require('react-intl/locale-data/es');
+  const fr = require('react-intl/locale-data/fr');
+  const pt = require('react-intl/locale-data/pt');
+  const ro = require('react-intl/locale-data/ro');
+  [cs, de, en, es, fr, pt, ro].forEach(locale => addLocaleData(locale));
+
   require('./main');
 };
+
 // github.com/andyearnshaw/Intl.js/#intljs-and-browserifywebpack
 if (!window.Intl) {
-  // App supported locales are defined in src/server/config.js
-  // Note the explicit enumeration, that's because static analysis ftw.
   require.ensure([
     'intl',
     'intl/locale-data/jsonp/cs.js',
--- src/browser/main.js
+++ src/browser/main.js
@@ -6,20 +6,7 @@ import createEngine from 'redux-storage-engine-localstorage';
 import createRoutes from './createRoutes';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
-import { addLocaleData } from 'react-intl';
 import { routerMiddleware, syncHistoryWithStore } from 'react-router-redux';
-
-// App supported locales are defined in src/server/config.js
-// Note the explicit enumeration, that's because static analysis ftw.
-import cs from 'react-intl/locale-data/cs';
-import de from 'react-intl/locale-data/de';
-import en from 'react-intl/locale-data/en';
-import es from 'react-intl/locale-data/es';
-import fr from 'react-intl/locale-data/fr';
-import pt from 'react-intl/locale-data/pt';
-import ro from 'react-intl/locale-data/ro';
-
-[cs, de, en, es, fr, pt, ro].forEach(locale => addLocaleData(locale));
 
 const initialState = window.__INITIAL_STATE__;
 const reportingMiddleware = configureReporting({
