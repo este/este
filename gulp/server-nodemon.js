a6a079a722ad4479ae47321436bb9a50693b3d40
Tomas Kulich <tomas.kulich@gmail.com>
2016-05-25 19:17:40 +0200

    Fix executing nodemon on windows; Close #920

--- gulp/server-nodemon.js
+++ gulp/server-nodemon.js
@@ -1,13 +1,33 @@
 import bg from 'gulp-bg';
 import gulp from 'gulp';
 import path from 'path';
+import fs from 'fs';
+
+// because fs.existsSync is deprecated
+const existsSync = (fname) => {
+  try {
+    fs.statSync(fname);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
 
 gulp.task(
   'server-nodemon',
-  bg(
-    path.normalize('node_modules/.bin/nodemon'),
-    '--ignore',
-    'webpack-assets.json',
-    path.normalize('src/server')
-  )
+  (cb) => {
+    // windows uses nodemon.cmd
+    const nodemonPaths = ['node_modules/.bin/nodemon.cmd', 'node_modules/.bin/nodemon']
+      .map((npath) => path.normalize(npath));
+    const npath = nodemonPaths.find(existsSync);
+    if (npath == null) {
+      throw new Error(`cannot find nodemon executable; tried: ${nodemonPaths}`);
+    }
+    bg(
+      npath,
+      '--ignore',
+      'webpack-assets.json',
+      path.normalize('src/server')
+    )(cb);
+  }
 );
