aeb9bb8687f22f4989bb92d150527e90d1bba980
Daniel Steigerwald <daniel@steigerwald.cz>
2016-05-25 20:03:38 +0200

    Fix #920

--- gulp/server-nodemon.js
+++ gulp/server-nodemon.js
@@ -1,13 +1,33 @@
 import bg from 'gulp-bg';
+import fs from 'fs';
 import gulp from 'gulp';
 import path from 'path';
 
+const existsSync = path => {
+  try {
+    fs.statSync(path);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
 gulp.task(
   'server-nodemon',
-  bg(
-    path.normalize('node_modules/.bin/nodemon'),
-    '--ignore',
-    'webpack-assets.json',
-    path.normalize('src/server')
-  )
+  (cb) => {
+    const nodemonPaths = [
+      'node_modules/.bin/nodemon.cmd', // Windows uses nodemon.cmd
+      'node_modules/.bin/nodemon'
+    ].map((nodemonPath) => path.normalize(nodemonPath));
+    const nodemonPath = nodemonPaths.find(path => existsSync(path));
+    if (nodemonPath == null) {
+      throw new Error(`Cannot find nodemon executable; tried: ${nodemonPaths}`);
+    }
+    bg(
+      nodemonPath,
+      '--ignore',
+      'webpack-assets.json',
+      path.normalize('src/server')
+    )(cb);
+  }
 );
