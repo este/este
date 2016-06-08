17fb38813a8b843c889e8fa826e5d686fad4ff1a
Daniel Steigerwald <daniel@steigerwald.cz>
2016-06-09 01:33:25 +0200

    Set iPhone 5s as default simulator
    Design for the smallest mobile device first.

--- gulp/ios.js
+++ gulp/ios.js
@@ -4,4 +4,6 @@ import gulp from 'gulp';
 // If this doesn't work, while manual Xcode works, try:
 // 1) delete ios/build directory
 // 2) reset content and settings in iOS simulator
-gulp.task('ios', ['native'], bg('react-native', 'run-ios'));
+gulp.task('ios', ['native'], bg(
+  'react-native', 'run-ios', '--simulator', 'iPhone 5s'
+));
