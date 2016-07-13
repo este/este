03dad7a7870a177a21a809b2e59eac6bdc3ab282
Daniel Steigerwald <daniel@steigerwald.cz>
2016-07-13 02:45:13 +0200

    Ups

--- src/server/config.js
+++ src/server/config.js
@@ -6,7 +6,7 @@ nconf.env('__');
 
 // For local development, we can override defaults easily. Rename
 // src/common/_config.json to src/common/config.json and uncomment next line.
-nconf.file('src/common/config.json');
+// nconf.file('src/common/config.json');
 
 // Remember, never put secrets in the source code. Use environment variables for
 // production or src/common/config.json for development instead.
