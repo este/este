3e4d5836cdd378457cab19a7d3b2dd40ad3cf7f4
Daniel Steigerwald <daniel@steigerwald.cz>
2016-07-14 04:11:55 +0200

    React Native 0.29.1
    Note Android example is still not updated for FBSDK.
    https://github.com/este/este/issues/1000

--- android/app/src/main/java/com/este/MainActivity.java
+++ android/app/src/main/java/com/este/MainActivity.java
@@ -1,14 +1,6 @@
 package com.este;
 
 import com.facebook.react.ReactActivity;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
-import io.fixd.rctlocale.RCTLocalePackage;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends ReactActivity {
 
@@ -19,28 +11,5 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "Este";
-    }
-
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    /**
-     * A list of packages used by the app. If the app uses additional views
-     * or modules besides the default ones, add more packages here.
-     */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new VectorIconsPackage(),
-            new FBSDKPackage(),
-            new RCTLocalePackage()
-        );
     }
 }
