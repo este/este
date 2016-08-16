7c5202ed68cb2343b037b61eb779b51a5bf2fad0
Jan Maršíček <honzamar@seznam.cz>
2016-08-16 13:51:16 +0200

    Add Link component with inline styles

--- src/browser/auth/SignInPage.js
+++ src/browser/auth/SignInPage.js
@@ -2,6 +2,7 @@ import Helmet from 'react-helmet';
 import React from 'react';
 import SignIn from './SignIn';
 import linksMessages from '../../common/app/linksMessages';
+import { Link } from '../app/components';
 import { FormattedMessage } from 'react-intl';
 import { locationShape } from 'react-router';
 
@@ -22,11 +23,11 @@ class SignInPage extends React.Component {
           }
         </FormattedMessage>
         <p>
-          Este uses <a
-            href="https://firebase.google.com/docs/auth/"
+          Este uses <Link
+            to="https://firebase.google.com/docs/auth/"
             rel="noopener noreferrer"
             target="_blank"
-          >Firebase Authentication</a> because it saves a lot of time.
+          >Firebase Authentication</Link> because it saves a lot of time.
         </p>
         <SignIn location={location} />
       </div>
