9ac74272040bf6075512155099ba26cb05d290fb
Daniel Steigerwald <daniel@steigerwald.cz>
2016-08-28 01:27:29 +0200

    React Native 0.32 Close #1100
    - gitignore refactored for simpler merge with the new React Native
    version
    - Remove React Native react-intl and console.ignoredYellowBox
    workarounds, finally.

--- gulp/native-fix.js
+++ gulp/native-fix.js
@@ -1,8 +1,40 @@
-import gulp from 'gulp';
 import del from 'del';
+import fs from 'fs';
+import gulp from 'gulp';
 
 // github.com/facebook/react-native/issues/4062#issuecomment-164598155
-// Tested with the React Native 0.30
+// This is a very long-term issue. Nobody can fix the whole NPM.
+const fixBabel5RcFiles = () =>
+  del(['node_modules/**/.babelrc', '!node_modules/react-native/**']);
+
+// github.com/yahoo/react-intl/issues/620#issuecomment-242897276
+// There is a chance react-intl team will fix it, but we can't wait.
+const fixReactIntlLibraries = () => {
+  [
+    ['react-intl', './locale-data/index.js'],
+    ['intl-messageformat', './lib/locales'],
+    ['intl-relativeformat', './lib/locales'],
+    ['intl', './locale-data/complete.js'],
+  ].forEach(([packageName, fileToFix]) => {
+    try {
+      const path = `node_modules/${packageName}/package.json`;
+      const file = fs.readFileSync(path, 'utf8');
+      const json = {
+        ...JSON.parse(file),
+        'react-native': {
+          [fileToFix]: fileToFix,
+        },
+      };
+      const jsonString = JSON.stringify(json, null, 2);
+      fs.writeFileSync(path, jsonString);
+    } catch (e) {
+      // If reading, parsing, or writing file fails, there's not much we can do.
+      return;
+    }
+  });
+};
+
 gulp.task('native-fix', async () => {
-  await del(['node_modules/**/.babelrc', '!node_modules/react-native/**']);
+  await fixBabel5RcFiles();
+  fixReactIntlLibraries();
 });
--- src/native/index.js
+++ src/native/index.js
@@ -1,50 +1,8 @@
-/* eslint-disable no-console, react/require-extension */
+/* eslint-disable react/require-extension */
 // Bootstrap environment
-require('react-native-browser-polyfill');
+const locales = require('./initialState.js').locales;
+const polyfillLocales = require('../server/intl/polyfillLocales');
 
-// github.com/facebook/react-native/issues/9093
-// Will be removed in two weeks.
-console.ignoredYellowBox = [
-  'Warning: You are manually calling a React.PropTypes validation',
-];
+polyfillLocales(global, locales);
 
-// global.Intl Polyfill
-// Server polyfillLocales doesn't work anymore, because packager error:
-// Encountered an error while persisting cache:
-// > Error: TimeoutError: transforming /Users/este/dev/este/node_modules
-// /intl/locale-data/complete.js took longer than 301 seconds.
-// require('../server/intl/polyfillLocales')(
-//   self,
-//   require('./initialState.js').locales
-// );
-// Workaround:
-// Remove "require('./locale-data/complete.js');" from node_modules/intl/index
-// We can't change that code, but we can reimplement it easily.
-// Expose `IntlPolyfill` as global to add locale data into runtime later on.
-global.IntlPolyfill = require('../../node_modules/intl/lib/core.js');
-
-global.Intl = global.IntlPolyfill;
-global.IntlPolyfill.__applyLocaleSensitivePrototypes();
-// App locales are defined in src/server/config.js
-require('../../node_modules/intl/locale-data/jsonp/cs.js');
-require('../../node_modules/intl/locale-data/jsonp/de.js');
-require('../../node_modules/intl/locale-data/jsonp/en.js');
-require('../../node_modules/intl/locale-data/jsonp/es.js');
-require('../../node_modules/intl/locale-data/jsonp/fr.js');
-require('../../node_modules/intl/locale-data/jsonp/pt.js');
-require('../../node_modules/intl/locale-data/jsonp/ro.js');
-
-const cs = require('react-intl/locale-data/cs');
-const de = require('react-intl/locale-data/de');
-const en = require('react-intl/locale-data/en');
-const es = require('react-intl/locale-data/es');
-const fr = require('react-intl/locale-data/fr');
-const pt = require('react-intl/locale-data/pt');
-const ro = require('react-intl/locale-data/ro');
-const { addLocaleData } = require('react-intl');
-
-[cs, de, en, es, fr, pt, ro].forEach(locale => addLocaleData(locale));
-
-// TODO: Consider.
-// self.Promise = require('../common/configureBluebird');
 require('./main');
