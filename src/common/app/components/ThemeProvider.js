b0e731e823a3c372a58530b2e428bbed3f63279b
Daniel Steigerwald <daniel@steigerwald.cz>
2016-09-07 04:05:03 +0200

    Soon we will have isomorphic responsive styles
    Without the server side rendering, we can use responsive Reflexbox now.

--- src/common/app/components/ThemeProvider.js
+++ src/common/app/components/ThemeProvider.js
@@ -11,12 +11,22 @@ class ThemeProvider extends React.Component {
 
   static childContextTypes = {
     rebass: React.PropTypes.object,
+    // reflexbox: React.PropTypes.object,
   };
 
   getChildContext() {
     const { theme } = this.props;
     return {
       rebass: theme,
+      // TODO: We are not there yet. github.com/este/este/issues/1120
+      // reflexbox: {
+      //   breakpoints: {
+      //     // github.com/twbs/bootstrap/blob/v4-dev/scss/_variables.scss#L126
+      //     sm: '(min-width: 0)',
+      //     md: '(min-width: 544px)',
+      //     lg: '(min-width: 768px)'
+      //   }
+      // }
     };
   }
 
