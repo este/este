b0497c0ad3802d58fe696615c5e378b3415d1581
Daniel Steigerwald <daniel@steigerwald.cz>
2016-09-09 00:27:09 +0200

    Add missing default
    It’s obvious we need integration tests.

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ const WebpackIsomorphicTools = require('webpack-isomorphic-tools');
 const config = require('./config').default;
 const polyfillLocales = require('./intl/polyfillLocales');
 const rootDir = require('path').resolve(__dirname, '..', '..');
-const webpackIsomorphicAssets = require('../../webpack/assets');
+const webpackIsomorphicAssets = require('../../webpack/assets').default;
 
 if (!process.env.NODE_ENV) {
   throw new Error(
