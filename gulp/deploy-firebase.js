309d9d0cbae939e9821e2b4ea2a0bb284f56084b
Theo Gregoire <Theo Gregoire>
2016-09-14 16:59:48 +0100

    Package update to gulp files to counteract https://github.com/este/este/issues/1134

--- gulp/deploy-firebase.js
+++ gulp/deploy-firebase.js
@@ -1,4 +1,5 @@
-import spawn from 'cross-spawn'; // Package used to counteract https://github.com/este/este/issues/1134
+/* Package used to counteract https://github.com/este/este/issues/1134 */
+import spawn from 'cross-spawn';
 import gulp from 'gulp';
 
 gulp.task('deploy-firebase', ['to-html'], done => {
