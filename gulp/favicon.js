4ee51cccf702752db2c9e4e80f16cdc4cbc8b69d
Sebastien Dubois <sdubois@protonmail.com>
2016-09-17 15:12:52 +0300

    Clean generated favicons before regenerating

--- gulp/favicon.js
+++ gulp/favicon.js
@@ -2,5 +2,5 @@ import gulp from 'gulp';
 import runSequence from 'run-sequence';
 
 gulp.task('favicon', done => {
-  runSequence('favicon-generate', 'favicon-inject', done);
+  runSequence('favicon-clean', 'favicon-generate', 'favicon-inject', done);
 });
