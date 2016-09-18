cd09d77f8e369806e84f3028a5d5b49de3d40a05
Daniel Steigerwald <daniel@steigerwald.cz>
2016-09-19 01:10:38 +0200

    Fix native NotFound

--- src/native/notfound/NotFoundPage.js
+++ src/native/notfound/NotFoundPage.js
@@ -2,9 +2,8 @@
 import React from 'react';
 import messages from '../../common/notfound/messages';
 import theme from '../app/themes/initial';
-import { Button, CenteredContainer, FormattedMessage } from '../app/components';
+import { CenteredContainer, FormattedMessage, Link } from '../app/components';
 import { StyleSheet } from 'react-native';
-import { connect } from 'react-redux';
 
 const styles = StyleSheet.create({
   heading: {
@@ -19,21 +18,14 @@ const styles = StyleSheet.create({
   },
 });
 
-// Use NotFoundPage for missing dynamic data. For non existing static page, use
-// redirect to home rather.
 const NotFoundPage = () => (
   <CenteredContainer>
     <FormattedMessage {...messages.h1} style={styles.heading} />
     <FormattedMessage {...messages.p} style={styles.paragraph} />
-    {/* onPress={() => selectTab('home')} */}
-    <Button>
+    <Link to="/">
       <FormattedMessage {...messages.continue} style={styles.button} />
-    </Button>
+    </Link>
   </CenteredContainer>
 );
-
-NotFoundPage.propTypes = {
-  selectTab: React.PropTypes.func.isRequired,
-};
 
 export default NotFoundPage;
