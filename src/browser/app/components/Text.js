84fbb78d16abd86eb628daa33251876ce6b66317
Daniel Steigerwald <daniel@steigerwald.cz>
2016-12-02 00:33:55 +0100

    Improve Text.js

--- src/browser/app/components/Text.js
+++ src/browser/app/components/Text.js
@@ -7,16 +7,22 @@ export type TextProps = {
   bold?: boolean,
   inverted?: boolean,
   small?: boolean,
+  transform?: 'none' | 'capitalize' | 'uppercase' | 'lowercase',
 };
 
+// Style is the transformation of props and theme.
 export const textStyle = (props: TextProps, theme: Theme) => ({
   color: props.inverted ? theme.colors.white : theme.colors.black,
   fontFamily: theme.fontFamily,
-  fontSize: `${props.small ? theme.fontSizes.smallText : theme.fontSizes.text}px`,
+  fontSize: props.small ? theme.fontSizes.smallText : theme.fontSizes.text,
   fontWeight: props.bold ? theme.bold : 'normal',
   lineHeight: theme.lineHeight,
+  textTransform: props.transform || 'none',
 });
 
+// Must be span, not p, because p can't be nested.
 const Text = style(textStyle, 'span');
 
+// Export Text wrapped in function because we want flow props check.
+// Such design is the must, beacuse it allows us to reuse styles.
 export default (props: TextProps) => <Text {...props} />;
