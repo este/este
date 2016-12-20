c5610aac636549c2bcd170debd25118136ad6c12
Cavitt <cavitt.glover@gmail.com>
2016-12-20 17:25:16 -0600

    Import only used rxjs/firebase features

--- src/common/configureEpics.js
+++ src/common/configureEpics.js
@@ -1,5 +1,11 @@
 /* @flow weak */
-import 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/catch';
+
 import { combineEpics } from 'redux-observable';
 import { epics as appEpics } from './app/actions';
 import { epics as authEpics } from './auth/actions';
