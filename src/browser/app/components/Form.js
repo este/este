420bbf8eb61fa4e499e87c9bd2bff34c01ec9a89
Daniel Steigerwald <daniel@steigerwald.cz>
2016-12-23 00:38:18 +0100

    Form component

---
+++ src/browser/app/components/Form.js
@@ -0,0 +1,31 @@
+/* @flow */
+import type { BoxProps } from './Box';
+import type { Styled } from '../themes/types';
+import Box from './Box';
+import styled from './styled';
+import React from 'react';
+
+type OnSubmit = SyntheticEvent => void;
+
+type FormProps = BoxProps & {
+  onSubmit?: OnSubmit,
+};
+
+const Form: Styled<FormProps> = styled(() => ({
+  $extends: Box,
+}), 'form', ['onSubmit']);
+
+const onSubmitWithPreventDefault = (onSubmit: OnSubmit) => e => {
+  if (!onSubmit) return;
+  e.preventDefault();
+  onSubmit(e);
+};
+
+const Wrapped: Styled<FormProps> = ({ onSubmit, ...props}) => (
+  <Form
+    onSubmit={onSubmitWithPreventDefault(onSubmit)}
+    {...props}
+  />
+);
+
+export default Wrapped;
