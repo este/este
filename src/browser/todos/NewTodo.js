1b4a2617b99e9ae7cc20f2a9ea6c87f853781bea
Daniel Steigerwald <daniel@steigerwald.cz>
2016-12-23 00:39:07 +0100

    NewTodo wip

--- src/browser/todos/NewTodo.js
+++ src/browser/todos/NewTodo.js
@@ -1,13 +1,57 @@
-// /* @flow */
-// import R from 'ramda';
-// import React from 'react';
-// import newTodoMessages from '../../common/todos/newTodoMessages';
-// import { Input, Form } from '../app/components';
-// import { addTodo } from '../../common/todos/actions';
-// import { connect } from 'react-redux';
-// import { fields } from '../../common/lib/redux-fields';
-// import { injectIntl, intlShape } from 'react-intl';
-//
+/* @flow */
+import R from 'ramda';
+import React from 'react';
+import newTodoMessages from '../../common/todos/newTodoMessages';
+import { addTodo } from '../../common/todos/actions';
+import { connect } from 'react-redux';
+import { fields } from '../../common/lib/redux-fields';
+import { injectIntl } from 'react-intl';
+import {
+  Input,
+  Form,
+  Text,
+} from '../app/components';
+
+type NewTodoProps = {|
+  addTodo: typeof addTodo,
+  fields: any,
+  intl: any,
+|};
+
+const NewTodo = ({ addTodo, fields, intl }: NewTodoProps) => (
+  <Form onSubmit={e => {
+    // addTodo(1)
+  }}>
+    <Input
+      // label="Input"
+      // labelSize={1}
+      // placeholder="Lorem ipsum dolor"
+    />
+    {/* Fok
+    {addTodo('d')} */}
+    {/* <Input
+      {...fields.title}
+      // label=""
+      // maxLength={100}
+      // onKeyDown={onInputKeyDown}
+      // placeholder={intl.formatMessage(newTodoMessages.placeholder)}
+    /> */}
+  </Form>
+);
+
+export default R.compose(
+  connect(
+    null,
+    { addTodo },
+  ),
+  injectIntl,
+  fields({
+    path: 'newTodo',
+    fields: ['title'],
+  }),
+)(NewTodo);
+
+
 // const NewTodo = ({ addTodo, fields, intl }) => {
 //   const onInputKeyDown = (event) => {
 //     if (event.key !== 'Enter') return;
@@ -15,34 +59,3 @@
 //     addTodo(fields.title.value);
 //     fields.$reset();
 //   };
-//
-//   return (
-//     <Form small>
-//       <Input
-//         {...fields.title}
-//         label=""
-//         maxLength={100}
-//         onKeyDown={onInputKeyDown}
-//         placeholder={intl.formatMessage(newTodoMessages.placeholder)}
-//       />
-//     </Form>
-//   );
-// };
-//
-// NewTodo.propTypes = {
-//   addTodo: React.PropTypes.func.isRequired,
-//   fields: React.PropTypes.object.isRequired,
-//   intl: intlShape.isRequired,
-// };
-//
-// export default R.compose(
-//   connect(
-//     null,
-//     { addTodo },
-//   ),
-//   injectIntl,
-//   fields({
-//     path: 'newTodo',
-//     fields: ['title'],
-//   }),
-// )(NewTodo);
--- src/browser/todos/TodosPage.js
+++ src/browser/todos/TodosPage.js
@@ -1,24 +1,29 @@
-// /* @flow */
+/* @flow */
+import NewTodo from './NewTodo';
+import React from 'react';
+import linksMessages from '../../common/app/linksMessages';
+import { injectIntl } from 'react-intl';
+import {
+  Box,
+  PageHeader,
+  Title,
+} from '../app/components';
+
 // import Buttons from './Buttons';
-// import NewTodo from './NewTodo';
-// import React from 'react';
 // import Todos from './Todos';
-// import linksMessages from '../../common/app/linksMessages';
-// import { PageHeader, Title, View } from '../app/components';
-// import { injectIntl, intlShape } from 'react-intl';
-//
-// const TodosPage = ({ intl }) => (
-//   <View>
-//     <Title message={linksMessages.todos} />
-//     <PageHeader heading={intl.formatMessage(linksMessages.todos)} />
-//     <NewTodo />
-//     <Todos />
-//     <Buttons />
-//   </View>
-// );
-//
-// TodosPage.propTypes = {
-//   intl: intlShape,
-// };
-//
-// export default injectIntl(TodosPage);
+
+type TodosPageProps = {
+  intl: any,
+};
+
+const TodosPage = ({ intl }: TodosPageProps) => (
+  <Box>
+    <Title message={linksMessages.todos} />
+    <PageHeader heading={intl.formatMessage(linksMessages.todos)} />
+    <NewTodo />
+    {/* <Todos />
+    <Buttons /> */}
+  </Box>
+);
+
+export default injectIntl(TodosPage);
