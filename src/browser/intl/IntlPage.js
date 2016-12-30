6b8717e01b920dd05c81f264cab97084fd1853b4
Daniel Steigerwald <daniel@steigerwald.cz>
2016-12-30 05:41:09 +0100

    IntlPage

--- src/browser/intl/IntlPage.js
+++ src/browser/intl/IntlPage.js
@@ -1,77 +1,63 @@
 // @flow
 import React from 'react';
+import SwitchLocale from './SwitchLocale';
 import linksMessages from '../../common/app/linksMessages';
 import {
+  FormattedDate,
+  FormattedMessage,
+  FormattedNumber,
+  FormattedRelative,
+  defineMessages,
+} from 'react-intl';
+import {
   Box,
-  Title,
   PageHeader,
+  Paragraph,
+  Title,
 } from '../app/components';
 
+const messages = defineMessages({
+  unreadCount: {
+    defaultMessage: `{unreadCount, plural,
+      one {message}
+      other {messages}
+    }`,
+    id: 'intl.page.unreadCount',
+  },
+});
 
-const IntlPage = () => (
-  <Box>
-    <Title message={linksMessages.intl} />
-    <PageHeader
-      heading="react-intl"
-    />
-  </Box>
-);
+const IntlPage = () => {
+  const renderedAt = Date.now();
+  const unreadCount = 123;
+
+  return (
+    <Box>
+      <Title message={linksMessages.intl} />
+      <PageHeader
+        heading="react-intl"
+      />
+      <SwitchLocale />
+      <Paragraph>
+        <FormattedDate
+          day="numeric"
+          month="short"
+          value={renderedAt}
+          year="numeric"
+        />
+      </Paragraph>
+      <Paragraph>
+        <FormattedNumber value={unreadCount} />{' '}
+        <FormattedMessage {...messages.unreadCount} values={{ unreadCount }} />
+      </Paragraph>
+      <Paragraph>
+        <FormattedRelative
+          initialNow={renderedAt}
+          updateInterval={1000 * 1}
+          value={renderedAt}
+        />
+      </Paragraph>
+    </Box>
+  );
+};
 
 export default IntlPage;
-
-// import Locales from './SwitchLocale';
-// import { Block, PageHeader, Text, Title, View } from '../app/components';
-// import {
-//   FormattedDate,
-//   FormattedMessage,
-//   FormattedNumber,
-//   FormattedRelative,
-//   defineMessages,
-// } from 'react-intl';
-//
-// const messages = defineMessages({
-//   unreadCount: {
-//     defaultMessage: `{unreadCount, plural,
-//       one {message}
-//       other {messages}
-//     }`,
-//     id: 'intl.page.unreadCount',
-//   },
-// });
-//
-// const IntlPage = () => {
-//   const componentRenderedAt = Date.now();
-//   const unreadCount = 123;
-//
-//   return (
-//     <View>
-//       <Title message={linksMessages.intl} />
-//       <PageHeader heading="react-intl" />
-//       <Locales />
-//       <Block>
-//         <Text>
-//           <FormattedDate
-//             value={Date.now()}
-//             day="numeric"
-//             month="long"
-//             year="numeric"
-//             formatMatcher="basic" // while this bug remains in react-intl: https://github.com/andyearnshaw/Intl.js/issues/179
-//           />
-//         </Text>
-//         <Text>
-//           <FormattedNumber value={unreadCount} /> {' '}
-//           <FormattedMessage {...messages.unreadCount} values={{ unreadCount }} />
-//         </Text>
-//         <Text>
-//           <FormattedRelative
-//             initialNow={componentRenderedAt}
-//             updateInterval={1000 * 1}
-//             value={componentRenderedAt}
-//           />
-//         </Text>
-//       </Block>
-//     </View>
-//   );
-// };
-//
-// export default IntlPage;
--- src/browser/intl/SwitchLocale.js
+++ src/browser/intl/SwitchLocale.js
@@ -1,36 +1,50 @@
 // @flow
-// import type { State } from '../../common/types';
-// import React from 'react';
-// import { Button, View } from '../app/components';
-// import { connect } from 'react-redux';
-// import { setCurrentLocale } from '../../common/intl/actions';
-//
-// const SwitchLocale = ({ currentLocale, locales, setCurrentLocale }) => (
-//   <View>
-//     {locales.map(locale =>
-//       <Button
-//         key={locale}
-//         mb={1}
-//         mr={1}
-//         onClick={() => setCurrentLocale(locale)}
-//         theme={locale === currentLocale ? 'primary' : 'secondary'}
-//       >
-//         {locale}
-//       </Button>,
-//     )}
-//   </View>
-// );
-//
-// SwitchLocale.propTypes = {
-//   currentLocale: React.PropTypes.string.isRequired,
-//   locales: React.PropTypes.arrayOf(React.PropTypes.string),
-//   setCurrentLocale: React.PropTypes.func.isRequired,
-// };
-//
-// export default connect(
-//   (state: State) => ({
-//     currentLocale: state.intl.currentLocale,
-//     locales: state.intl.locales,
-//   }),
-//   { setCurrentLocale },
-// )(SwitchLocale);
+import type { State } from '../../common/types';
+import React from 'react';
+import compose from 'ramda/src/compose';
+import { connect } from 'react-redux';
+import { setCurrentLocale } from '../../common/intl/actions';
+import {
+  Box,
+  Button,
+} from '../app/components';
+
+type SwitchLocaleProps = {
+  currentLocale: string,
+  locales: Array<string>,
+  setCurrentLocale: typeof setCurrentLocale,
+};
+
+const SwitchLocale = ({
+  currentLocale,
+  locales,
+  setCurrentLocale,
+}: SwitchLocaleProps) => (
+  <Box
+    marginBottom={1}
+    marginHorizontal={-0.25}
+  >
+    {locales.map(locale =>
+      <Button
+        active={locale === currentLocale}
+        display="inline-block"
+        key={locale}
+        marginHorizontal={0.25}
+        onClick={() => setCurrentLocale(locale)}
+        primary
+      >
+        {locale}
+      </Button>,
+    )}
+  </Box>
+);
+
+export default compose(
+  connect(
+    (state: State) => ({
+      currentLocale: state.intl.currentLocale,
+      locales: state.intl.locales,
+    }),
+    { setCurrentLocale },
+  ),
+)(SwitchLocale);
