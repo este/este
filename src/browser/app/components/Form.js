2063b4e0fba192efc624c859f8ac53511e764c87
Daniel Steigerwald <daniel@steigerwald.cz>
2016-12-31 07:40:20 +0100

    Add ugly hack for Form

--- src/browser/app/components/Form.js
+++ src/browser/app/components/Form.js
@@ -6,6 +6,7 @@ import styled from './styled';
 import React from 'react';
 
 type FormProps = BoxProps & {
+  children?: any,
   onSubmit?: SyntheticEvent => void,
 };
 
@@ -26,12 +27,23 @@ const StyledForm: Styled<FormProps> = styled((theme, {
   }],
 }), 'form', ['onSubmit']);
 
+const invisibleSubmitToAllowSubmitOnInputEnterStyle = { display: 'none' };
+
 // Feel
-const Form: Styled<FormProps> = ({ onSubmit, ...props }) => (
+const Form: Styled<FormProps> = ({ children, onSubmit, ...props }) => (
   <StyledForm
     onSubmit={onSubmitWithPreventDefault(onSubmit)}
     {...props}
-  />
+  >
+    {/* This is hack allowing to submit form on enter keypress in input. */}
+    {/* We need that because button is rendered as div. */}
+    {/* Button is rendered as div to simplify text-like look styling. */}
+    <input
+      style={invisibleSubmitToAllowSubmitOnInputEnterStyle}
+      type="submit"
+    />
+    {children}
+  </StyledForm>
 );
 
 export default Form;
