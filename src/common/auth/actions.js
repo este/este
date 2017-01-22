79317288ad5606db374a4e9a93e582df95e7290b
Daniel Steigerwald <daniel@steigerwald.cz>
2017-01-22 07:25:01 +0100

    Update koa-webpack, Close #1295

--- src/common/auth/actions.js
+++ src/common/auth/actions.js
@@ -96,6 +96,7 @@ const signInEpic = (
 ) => {
   // groups.google.com/forum/#!msg/firebase-talk/643d_lwUAMI/bfQyn8D-BQAJ
   // stackoverflow.com/a/33997042/233902
+  // It's probably fixed: github.com/este/este/issues/1295
   const isMobileFacebookApp = () => {
     const ua = navigator.userAgent || navigator.vendor; // eslint-disable-line no-undef
     return ua.indexOf('FBAN') > -1 || ua.indexOf('FBAV') > -1;
