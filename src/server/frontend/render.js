9ee142edfb459d17cd439dc067bbe5a22a175929
Cavitt <cavitt.glover@gmail.com>
2017-02-15 18:39:20 +0900

    Return supported languages in header, new getLocale order
    First check cookies for user setting
    Then check request header for browser setting
    Finally default to default language

--- src/server/frontend/render.js
+++ src/server/frontend/render.js
@@ -24,7 +24,7 @@ const settleAllWithTimeout = promises => Promise
   .catch((error) => {
     // $FlowFixMe
     if (error instanceof Promise.TimeoutError) {
-      console.log('Server fetch timeouted:', error);
+      console.log('Server fetch timed out:', error);
       return;
     }
     throw error;
@@ -36,9 +36,12 @@ const initialState = createInitialState();
 const getHost = req =>
   `${req.headers['x-forwarded-proto'] || req.protocol}://${req.headers.host}`;
 
-const getLocale = req => process.env.IS_SERVERLESS
-  ? config.defaultLocale
-  : req.acceptsLanguages(config.locales) || config.defaultLocale;
+const getLocale = req =>
+  process.env.IS_SERVERLESS
+    ? config.defaultLocale // No SSR, default to English
+    : (req.cookies && req.cookies.locale) // User set language explicitly in app
+      || req.acceptsLanguages(config.locales) // Browser specified language
+      || config.defaultLocale; // No preference found, default to English
 
 const createStore = req => configureStore({
   initialState: {
@@ -104,6 +107,7 @@ const renderHtml = (state, body) => {
 };
 
 // react-router.now.sh/ServerRouter
+const localeList = config.locales.join(', ');
 const render = async (req: Object, res: Object, next: Function) => {
   try {
     const context = createServerRenderContext();
@@ -112,6 +116,8 @@ const render = async (req: Object, res: Object, next: Function) => {
 
     let body = renderBody(store, context, req.url, fetchPromises);
     const result = context.getResult();
+
+    res.header('Content-Language', localeList);
 
     if (result.redirect) {
       res.redirect(301, result.redirect.pathname + result.redirect.search);
