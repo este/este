b735feef292a278c3c63b42ed02f256c0e52fceb
Cavitt <cavitt.glover@gmail.com>
2017-03-08 08:19:46 +0900

    RxJS feature imports

--- src/common/configureEpics.js
+++ src/common/configureEpics.js
@@ -1,5 +1,14 @@
 // @flow
-import 'rxjs';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/merge';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/catch';
 import { combineEpics } from 'redux-observable';
 import { epics as appEpics } from './app/actions';
 import { epics as authEpics } from './auth/actions';
