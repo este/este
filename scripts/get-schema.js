a17d2d442b6c0d9e9c82860be2b8c5f2084e2586
Daniel Steigerwald <daniel@steigerwald.cz>
2017-05-13 04:11:37 +0200

    For future Relay Modern

---
+++ scripts/get-schema.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const {
+  buildClientSchema,
+  introspectionQuery,
+  printSchema,
+} = require('graphql/utilities');
+
+fetch('https://api.graph.cool/relay/v1/cj157d6lgacrd01553jnd34tv', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ query: introspectionQuery }),
+})
+  .then(res => res.json())
+  .then(res => {
+    const schemaString = printSchema(buildClientSchema(res.data));
+    fs.writeFileSync('./data/schema.graphql', schemaString);
+  });
