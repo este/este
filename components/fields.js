73e43c11cb63753e83667aba5c52ed7dddd2aa63
Daniel Steigerwald <daniel@steigerwald.cz>
2017-05-18 01:15:54 +0200

    fields hoc
    So this is super minimal typed implementation or previous Este fields.
    But, it is really abstraction do we need?

--- components/fields.js
+++ components/fields.js
@@ -1,33 +1,54 @@
 // @flow
-import type { Fields, FunctionalComponent, State, Store } from '../types';
+import type { FieldName, FunctionalComponent, State, Store } from '../types';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { setField } from '../lib/fields/actions';
 
-type FieldNames = Array<$Keys<Fields>>;
+type FieldNames = Array<FieldName>;
 
 type GetId = (props: Object) => string;
 
 type WrappedComponent = FunctionalComponent<{
-  // TODO: Restrict type Fields by passed fieldNames somehow.
+  // TODO: Restrict FieldName by passed fieldNames somehow.
   fields: {
-    [$Keys<Fields>]: {
-      name: $Keys<Fields>,
-      value: any,
-      onChange: any,
-    },
+    [fieldName: FieldName]: { name: FieldName, value: any, onChange: any },
   },
 }>;
 
 type FieldsContext = { store: Store };
 
+const createFieldsProps = (
+  fieldNames: FieldNames,
+  id: string,
+  props: Object,
+  context: FieldsContext
+) => {
+  const { store } = context;
+  const { initial, changed } = store.getState().fields;
+  const changedById = changed[id];
+  return fieldNames.reduce((props, fieldName) => {
+    const fieldValue = changedById && changedById.hasOwnProperty(fieldName)
+      ? changedById[fieldName]
+      : initial[fieldName];
+    return {
+      ...props,
+      [fieldName]: {
+        name: fieldName,
+        value: fieldValue,
+        onChange: (e: Event & { currentTarget: { value: mixed } }) => {
+          const action = setField(id, fieldName, e.currentTarget.value);
+          store.dispatch(action);
+        },
+      },
+    };
+  }, {});
+};
+
 const fields = (fieldNames: FieldNames, getId: GetId = props => '') => (
   WrappedComponent: WrappedComponent
 ) =>
   class Fields extends React.Component {
-    static contextTypes = {
-      store: PropTypes.object, // Redux store.
-    };
+    static contextTypes = { store: PropTypes.object };
 
     state: { fields: Object };
 
@@ -35,44 +56,32 @@ const fields = (fieldNames: FieldNames, getId: GetId = props => '') => (
 
     context: FieldsContext;
 
-    static createFieldsProps = (props: Object, context: FieldsContext) => {
-      const { initial, changed } = context.store.getState().fields;
-      const id = getId(props);
-      const idChanged = changed[id];
-      return fieldNames.reduce((props, fieldName) => {
-        const fieldValue = idChanged && idChanged.hasOwnProperty(fieldName)
-          ? idChanged[fieldName]
-          : initial[fieldName];
-        return {
-          ...props,
-          [fieldName]: {
-            name: fieldName,
-            value: fieldValue,
-            onChange: (e: Event & { currentTarget: { value: mixed } }) => {
-              const action = setField(id, fieldName, e.currentTarget.value);
-              context.store.dispatch(action);
-            },
-          },
-        };
-      }, {});
-    };
-
     constructor(props: Object, context: FieldsContext) {
       super(props);
       this.state = {
-        fields: Fields.createFieldsProps(props, context),
+        fields: createFieldsProps(fieldNames, getId(props), props, context),
       };
     }
 
     componentDidMount() {
       const { store } = this.context;
       this.unsubscribe = store.subscribe(() => {
-        // const changed = store.getState().fields.changed[getId];
-        // if (!changed) return;
-        // porovnat fieldNames se state vysledkem, pokud nesedi..
-        // pokud ma id changed, a prop se nerovna initial...
-        this.setState({
-          fields: Fields.createFieldsProps(this.props, this.context),
+        const { changed } = store.getState().fields;
+        const changedById = changed[getId(this.props)];
+        const stateFields = this.state.fields;
+        const hasChange =
+          changedById &&
+          fieldNames.some(
+            fieldName => stateFields[fieldName].value !== changedById[fieldName]
+          );
+        if (!hasChange) return;
+        this.setState(prevState => {
+          const newState = { ...prevState };
+          fieldNames.forEach(fieldName => {
+            if (!changedById.hasOwnProperty(fieldName)) return;
+            newState.fields[fieldName].value = changedById[fieldName];
+          });
+          return newState;
         });
       });
     }
