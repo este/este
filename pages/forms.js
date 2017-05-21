212d8a428846d3a26857f53b96944777e6c80e42
Daniel Steigerwald <daniel@steigerwald.cz>
2017-05-21 23:29:48 +0200

    eslint-config-airbnb 15

--- pages/forms.js
+++ pages/forms.js
@@ -14,7 +14,7 @@ import { connect } from 'react-redux';
 import { setUserForm } from '../lib/forms/actions';
 
 const UserForm = ({ id, user, setUserForm }) => {
-  // TODO: set and submit and connect as well should be abstracted.
+  // TODO: set and submit and connect as well could be abstracted.
   const set = prop => state => setUserForm(id, { ...user, [prop]: state });
   const reset = () => setUserForm(id, null);
   const submit = () => {
