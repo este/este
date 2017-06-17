0d234a6d96f1efb0288980d212b8a060bbc49d86
Daniel Steigerwald <daniel@steigerwald.cz>
2017-06-17 17:08:54 +0200

    Improve AutoFocus scrolling logic. Scroll less.

--- components/auto-focus.js
+++ components/auto-focus.js
@@ -34,11 +34,34 @@ class AutoFocus extends React.Component {
       el.tagName === 'BUTTON' ||
       (typeof el.tabIndex === 'number' && el.tabIndex > -1); // for div button
     if (focusable && typeof el.focus === 'function') {
+      // flow refinement
       el.focus();
-    } else if (typeof el.scrollIntoView === 'function') {
-      // TODO: Scroll as minimal as possible to ensure visibility only.
-      el.scrollIntoView({ behavior: 'smooth' });
+      return;
     }
+    if (
+      typeof el.scrollIntoView !== 'function' ||
+      typeof el.getBoundingClientRect !== 'function' ||
+      typeof el.scrollIntoView !== 'function'
+    ) {
+      return;
+    }
+    const yPadding = 10;
+    const rect = el.getBoundingClientRect();
+    const bottomOffset = rect.bottom - window.innerHeight;
+    if (bottomOffset > 0) {
+      window.scrollBy({
+        top: bottomOffset + yPadding,
+        left: 0,
+        behavior: 'smooth',
+      });
+      return;
+    }
+    if (rect.top > 0) return;
+    window.scrollBy({
+      top: rect.top - yPadding,
+      left: 0,
+      behavior: 'smooth',
+    });
   }
 
   render() {
