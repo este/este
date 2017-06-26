9a9b51a1e69eef3d6e77e140501bcfdc17470502
Daniel Steigerwald <daniel@steigerwald.cz>
2017-06-26 05:02:11 +0200

    Sort extracted messages

--- scripts/extract-messages.js
+++ scripts/extract-messages.js
@@ -10,13 +10,16 @@ const defaultMessages = glob
   .sync('./lang/.messages/**/*.json')
   .map(filename => readFileSync(filename, 'utf8'))
   .map(file => JSON.parse(file))
-  .reduce((messages, descriptors) => {
-    descriptors.forEach(({ id, defaultMessage }) => {
-      if (messages.hasOwnProperty(id)) {
-        throw new Error(`Duplicate message id: ${id}`);
-      }
-      messages[id] = defaultMessage;
-    });
+  .reduce(
+    (descriptors, fileDescriptors) => [...descriptors, ...fileDescriptors],
+    [],
+  )
+  .sort((a, b) => a.id.localeCompare(b.id))
+  .reduce((messages, descriptor) => {
+    if (messages.hasOwnProperty(descriptor.id)) {
+      throw new Error(`Duplicate message id: ${descriptor.id}`);
+    }
+    messages[descriptor.id] = descriptor.defaultMessage;
     return messages;
   }, {});
 
