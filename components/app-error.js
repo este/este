658788a5b1489b9c33e6a4671857d2c613eb3e79
Daniel Steigerwald <daniel@steigerwald.cz>
2017-06-27 04:45:51 +0200

    Localize app error

--- components/app-error.js
+++ components/app-error.js
@@ -1,21 +1,44 @@
 // @flow
 import type { AppError as AppErrorType } from '../types';
 import Text, { type TextProps } from './text';
+import { FormattedMessage } from 'react-intl';
 
 type AppErrorProps = TextProps & {
   error: ?AppErrorType,
+};
+
+const getAppErrorMessage = error => {
+  switch (error.type) {
+    case 'insufficientStorage':
+      return (
+        <FormattedMessage
+          defaultMessage="Insufficient storage."
+          id="error.insufficientStorage"
+        />
+      );
+    case 'xhrError':
+      return (
+        <FormattedMessage
+          defaultMessage="Network error. Please try it later."
+          id="error.xhrError"
+        />
+      );
+    default:
+      // eslint-disable-next-line no-unused-expressions
+      (error: empty);
+      return null;
+  }
 };
 
 const AppError = (props: AppErrorProps) => {
   const { error, bold = true, color = 'danger', ...restProps } = props;
   if (!error) return null;
 
-  // TODO: Return translated message.
   // TODO: Report unknown error to server.
-  const msg = JSON.stringify(error);
+  const message = getAppErrorMessage(error);
   return (
     <Text autoFocus={error} bold={bold} color={color} {...restProps}>
-      {msg}
+      {message}
     </Text>
   );
 };
