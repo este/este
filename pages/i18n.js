3c563f3c3a8cfca2dc3f28d898eb0afc853364f6
Daniel Steigerwald <daniel@steigerwald.cz>
2017-06-27 03:09:05 +0200

    Add some i18n examples

--- pages/i18n.js
+++ pages/i18n.js
@@ -1,15 +1,33 @@
 // @flow
 import Heading from '../components/heading';
+import P from '../components/p';
 import Page from '../components/page';
 import SwitchLocale from '../components/switch-locale';
 import app from '../components/app';
 import sitemap from '../lib/sitemap';
+import { FormattedDate, FormattedNumber, FormattedRelative } from 'react-intl';
 
-const Forms = ({ intl, url: { pathname } }) =>
-  <Page title={intl.formatMessage(sitemap.i18n.title)}>
-    <Heading size={3}>{intl.formatMessage(sitemap.i18n.title)}</Heading>
-    <SwitchLocale pathname={pathname} />
-
-  </Page>;
+const Forms = ({ intl, url: { pathname } }) => {
+  const now = Date.now();
+  return (
+    <Page title={intl.formatMessage(sitemap.i18n.title)}>
+      <Heading size={3}>{intl.formatMessage(sitemap.i18n.title)}</Heading>
+      <SwitchLocale pathname={pathname} />
+      <P>
+        <FormattedNumber value={1024} />
+      </P>
+      <P>
+        <FormattedDate day="numeric" month="long" value={now} year="numeric" />
+      </P>
+      <P>
+        <FormattedRelative
+          initialNow={now}
+          updateInterval={1000 * 1}
+          value={now}
+        />
+      </P>
+    </Page>
+  );
+};
 
 export default app(Forms);
