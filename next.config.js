bba8efd8cc5306c2672b2f14349f28c0deff1e7a
Daniel Steigerwald <daniel@steigerwald.cz>
2017-06-29 23:02:08 +0200

    Add webpack-bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // This file is not going through babel transformation.
 // So, we write it in vanilla JS.
 // (But you could use ES2015 features supported by your Node.js version)
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { assocPath } = require('ramda');
 
 const eslintRule = {
@@ -12,6 +13,15 @@ const eslintRule = {
 
 module.exports = {
   webpack: (config, { dev }) => {
+    if (process.env.ANALYZE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'server',
+          analyzerPort: 8888,
+          openAnalyzer: true,
+        }),
+      );
+    }
     // TODO: Figure out, how to render errors as warnings in dev mode.
     // https://github.com/MoOx/eslint-loader/issues/174
     if (dev) return config;
