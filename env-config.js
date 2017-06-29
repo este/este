5c61dcea70fff3ae97bd3153b410b081d9689ba1
Daniel Steigerwald <daniel@steigerwald.cz>
2017-06-30 01:56:51 +0200

    Use get-schema for versioning graph.cool

--- env-config.js
+++ env-config.js
@@ -13,6 +13,6 @@ module.exports = {
   DEFAULT_LOCALE: 'en',
   DOMAIN: production ? 'example.com' : 'localhost:3000/',
   GRAPHQL_ENDPOINT: production
-    ? 'https://api.graph.cool/simple/v1/cj157d6lgacrd01553jnd34tv'
-    : 'https://api.graph.cool/simple/v1/cj157d6lgacrd01553jnd34tv',
+    ? 'https://api.graph.cool/simple/v1/cj4iytrfz6tgx0192svkjh324'
+    : 'https://api.graph.cool/simple/v1/cj4iytrfz6tgx0192svkjh324',
 };
--- scripts/get-schema.js
+++ scripts/get-schema.js
@@ -1,23 +1,23 @@
-// TODO: Wait once Relay Modern be ready for SSR in Next.js
-// const fetch = require('node-fetch');
-// const fs = require('fs');
-//
-// const {
-//   buildClientSchema,
-//   introspectionQuery,
-//   printSchema,
-// } = require('graphql/utilities');
-//
-// fetch('https://api.graph.cool/relay/v1/cj157d6lgacrd01553jnd34tv', {
-//   method: 'POST',
-//   headers: {
-//     Accept: 'application/json',
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify({ query: introspectionQuery }),
-// })
-//   .then(res => res.json())
-//   .then(res => {
-//     const schemaString = printSchema(buildClientSchema(res.data));
-//     fs.writeFileSync('./data/schema.graphql', schemaString);
-//   });
+const fetch = require('node-fetch');
+const fs = require('fs');
+const { GRAPHQL_ENDPOINT } = require('../env-config');
+
+const {
+  buildClientSchema,
+  introspectionQuery,
+  printSchema,
+} = require('graphql/utilities');
+
+fetch(GRAPHQL_ENDPOINT, {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ query: introspectionQuery }),
+})
+  .then(res => res.json())
+  .then(res => {
+    const schemaString = printSchema(buildClientSchema(res.data));
+    fs.writeFileSync('./data/schema.graphql', schemaString);
+  });
