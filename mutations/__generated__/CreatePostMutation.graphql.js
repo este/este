a3d2f20fd6b88ab8e2f677cfbc83dbce7c1ccb2c
Daniel Steigerwald <daniel@steigerwald.cz>
2017-07-22 22:32:10 +0200

    yarn relay

--- mutations/__generated__/CreatePostMutation.graphql.js
+++ mutations/__generated__/CreatePostMutation.graphql.js
@@ -26,7 +26,6 @@ export type CreatePostMutationResponse = {|
 |};
 */
 
-
 /*
 mutation CreatePostMutation(
   $input: CreatePostInput!
@@ -41,128 +40,129 @@ mutation CreatePostMutation(
 */
 
 const batch /*: ConcreteBatch*/ = {
-  "fragment": {
-    "argumentDefinitions": [
+  fragment: {
+    argumentDefinitions: [
       {
-        "kind": "LocalArgument",
-        "name": "input",
-        "type": "CreatePostInput!",
-        "defaultValue": null
-      }
+        kind: 'LocalArgument',
+        name: 'input',
+        type: 'CreatePostInput!',
+        defaultValue: null,
+      },
     ],
-    "kind": "Fragment",
-    "metadata": null,
-    "name": "CreatePostMutation",
-    "selections": [
+    kind: 'Fragment',
+    metadata: null,
+    name: 'CreatePostMutation',
+    selections: [
       {
-        "kind": "LinkedField",
-        "alias": null,
-        "args": [
+        kind: 'LinkedField',
+        alias: null,
+        args: [
           {
-            "kind": "Variable",
-            "name": "input",
-            "variableName": "input",
-            "type": "CreatePostInput!"
-          }
+            kind: 'Variable',
+            name: 'input',
+            variableName: 'input',
+            type: 'CreatePostInput!',
+          },
         ],
-        "concreteType": "CreatePostPayload",
-        "name": "createPost",
-        "plural": false,
-        "selections": [
+        concreteType: 'CreatePostPayload',
+        name: 'createPost',
+        plural: false,
+        selections: [
           {
-            "kind": "LinkedField",
-            "alias": null,
-            "args": null,
-            "concreteType": "Post",
-            "name": "post",
-            "plural": false,
-            "selections": [
+            kind: 'LinkedField',
+            alias: null,
+            args: null,
+            concreteType: 'Post',
+            name: 'post',
+            plural: false,
+            selections: [
               {
-                "kind": "ScalarField",
-                "alias": null,
-                "args": null,
-                "name": "id",
-                "storageKey": null
+                kind: 'ScalarField',
+                alias: null,
+                args: null,
+                name: 'id',
+                storageKey: null,
               },
               {
-                "kind": "ScalarField",
-                "alias": null,
-                "args": null,
-                "name": "text",
-                "storageKey": null
-              }
+                kind: 'ScalarField',
+                alias: null,
+                args: null,
+                name: 'text',
+                storageKey: null,
+              },
             ],
-            "storageKey": null
-          }
+            storageKey: null,
+          },
         ],
-        "storageKey": null
-      }
+        storageKey: null,
+      },
     ],
-    "type": "Mutation"
+    type: 'Mutation',
   },
-  "id": null,
-  "kind": "Batch",
-  "metadata": {},
-  "name": "CreatePostMutation",
-  "query": {
-    "argumentDefinitions": [
+  id: null,
+  kind: 'Batch',
+  metadata: {},
+  name: 'CreatePostMutation',
+  query: {
+    argumentDefinitions: [
       {
-        "kind": "LocalArgument",
-        "name": "input",
-        "type": "CreatePostInput!",
-        "defaultValue": null
-      }
+        kind: 'LocalArgument',
+        name: 'input',
+        type: 'CreatePostInput!',
+        defaultValue: null,
+      },
     ],
-    "kind": "Root",
-    "name": "CreatePostMutation",
-    "operation": "mutation",
-    "selections": [
+    kind: 'Root',
+    name: 'CreatePostMutation',
+    operation: 'mutation',
+    selections: [
       {
-        "kind": "LinkedField",
-        "alias": null,
-        "args": [
+        kind: 'LinkedField',
+        alias: null,
+        args: [
           {
-            "kind": "Variable",
-            "name": "input",
-            "variableName": "input",
-            "type": "CreatePostInput!"
-          }
+            kind: 'Variable',
+            name: 'input',
+            variableName: 'input',
+            type: 'CreatePostInput!',
+          },
         ],
-        "concreteType": "CreatePostPayload",
-        "name": "createPost",
-        "plural": false,
-        "selections": [
+        concreteType: 'CreatePostPayload',
+        name: 'createPost',
+        plural: false,
+        selections: [
           {
-            "kind": "LinkedField",
-            "alias": null,
-            "args": null,
-            "concreteType": "Post",
-            "name": "post",
-            "plural": false,
-            "selections": [
+            kind: 'LinkedField',
+            alias: null,
+            args: null,
+            concreteType: 'Post',
+            name: 'post',
+            plural: false,
+            selections: [
               {
-                "kind": "ScalarField",
-                "alias": null,
-                "args": null,
-                "name": "id",
-                "storageKey": null
+                kind: 'ScalarField',
+                alias: null,
+                args: null,
+                name: 'id',
+                storageKey: null,
               },
               {
-                "kind": "ScalarField",
-                "alias": null,
-                "args": null,
-                "name": "text",
-                "storageKey": null
-              }
+                kind: 'ScalarField',
+                alias: null,
+                args: null,
+                name: 'text',
+                storageKey: null,
+              },
             ],
-            "storageKey": null
-          }
+            storageKey: null,
+          },
         ],
-        "storageKey": null
-      }
-    ]
+        storageKey: null,
+      },
+    ],
   },
-  "text": "mutation CreatePostMutation(\n  $input: CreatePostInput!\n) {\n  createPost(input: $input) {\n    post {\n      id\n      text\n    }\n  }\n}\n"
+  text:
+    'mutation CreatePostMutation(\n  $input: CreatePostInput!\n) {\n  createPost(input: $input) {\n    post {\n      id\n      text\n    }\n  }\n}\n',
 };
 
 module.exports = batch;
