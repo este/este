a248f3c2b2ab7a1ee530cb61819652535ef9f36e
Daniel Steigerwald <daniel@steigerwald.cz>
2017-08-25 01:51:23 +0200

    Add CountryFlag

---
+++ components/CountryFlag.js
@@ -0,0 +1,16 @@
+// @flow
+import React from 'react';
+
+// There is detect-emoji-support lib, but it does not work on the server.
+// We don't need it, emoji is well supported.
+// http://caniemoji.com
+
+// TODO: Return string with React 16
+const CountryFlag = ({ code }: { code: string }) =>
+  <span style={{ verticalAlign: 'middle' }}>
+    {code
+      .toUpperCase()
+      .replace(/./g, char => String.fromCodePoint(char.charCodeAt(0) + 127397))}
+  </span>;
+
+export default CountryFlag;
