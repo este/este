7d2f25b613efea3a9604f35784dada6a3b8bc57c
Daniel Steigerwald <daniel@steigerwald.cz>
2017-08-28 16:11:35 +0200

    Fun

--- pages/index.js
+++ pages/index.js
@@ -9,23 +9,34 @@ import withIsAuthenticated, {
 } from '../components/withIsAuthenticated';
 import A from '../components/A';
 import { FormattedMessage } from 'react-intl';
+import Blockquote from '../components/Blockquote';
+import P from '../components/P';
 
 const Index = ({ intl }, { isAuthenticated }: IsAuthenticatedContext) =>
   <Page title={intl.formatMessage(sitemap.index.title)}>
     <Heading size={3}>
       {intl.formatMessage(sitemap.index.title)}
     </Heading>
-    <A href={sitemap.me.path}>
-      {isAuthenticated
-        ? <FormattedMessage
-            defaultMessage="Manage your webs"
-            id="index.manageYourWebs"
-          />
-        : <FormattedMessage
-            defaultMessage="Create your first web"
-            id="index.createYourFirstWeb"
-          />}
-    </A>
+    <P>
+      <A href={sitemap.me.path}>
+        {isAuthenticated
+          ? <FormattedMessage
+              defaultMessage="Manage your webs"
+              id="index.manageYourWebs"
+            />
+          : <FormattedMessage
+              defaultMessage="Create your first web"
+              id="index.createYourFirstWeb"
+            />}
+      </A>
+    </P>
+    <Blockquote
+      source="Friedrich Hayek"
+      href="https://en.wikipedia.org/wiki/Friedrich_Hayek"
+    >
+      The curious task of economics is to demonstrate to men how little they
+      really know about what they imagine they can design.
+    </Blockquote>
   </Page>;
 
 withIsAuthenticated(Index);
