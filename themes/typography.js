74c08385689712907e657137a9c76a7f5dbc1d25
Romeo Van Snick <romeovs@gmail.com>
2017-10-10 20:01:29 +0200

    Simplify typography calculations

--- themes/typography.js
+++ themes/typography.js
@@ -33,17 +33,16 @@ const typography = ({
   fontSize: number,
   fontSizeScale: number | $Keys<typeof scale>,
   lineHeight: number,
-|}) => ({
-  fontSize: (level: number) =>
-    Array.from(Array(Math.abs(level))).reduce(size => {
-      const scaleRatio =
-        typeof fontSizeScale === 'string'
-          ? scale[fontSizeScale]
-          : fontSizeScale;
-      return level > 0 ? size * (1 / scaleRatio) : size / (1 / scaleRatio);
-    }, fontSize),
-  lineHeight,
-  rhythm: (ratio: number) => lineHeight * ratio,
-});
+|}) => {
+  const scaleRatio =
+    typeof fontSizeScale === 'string'
+      ? scale[fontSizeScale]
+      : fontSizeScale;
+  return {
+    fontSize: (level: number) => fontSize ** (scaleRatio, -level),
+    lineHeight,
+    rhythm: (ratio: number) => lineHeight * ratio,
+  };
+};
 
 export default typography;
