542833202730239f93eb6152742b05356b8b7dc1
Daniel Steigerwald <daniel@steigerwald.cz>
2017-11-04 00:35:22 +0100

    Remove unneeded wrapper from CountryFlag

--- components/CountryFlag.js
+++ components/CountryFlag.js
@@ -1,17 +1,9 @@
 // @flow
 import * as React from 'react';
 
-// There is detect-emoji-support lib, but it does not work on the server.
-// We don't need it, emoji is well supported.
-// http://caniemoji.com
-
-// TODO: Return string with React 16
-const CountryFlag = ({ code }: { code: string }) => (
-  <span style={{ verticalAlign: 'middle' }}>
-    {code
-      .toUpperCase()
-      .replace(/./g, char => String.fromCodePoint(char.charCodeAt(0) + 127397))}
-  </span>
-);
+const CountryFlag = ({ code }: { code: string }) =>
+  code
+    .toUpperCase()
+    .replace(/./g, char => String.fromCodePoint(char.charCodeAt(0) + 127397));
 
 export default CountryFlag;
