9ff3353748c525296cd62fbed9bdeed8d7929953
Daniel Steigerwald <daniel@steigerwald.cz>
2018-01-03 03:08:34 +0100

    Ignore metaKey in RovingTabIndex

--- components/RovingTabIndex.js
+++ components/RovingTabIndex.js
@@ -232,6 +232,8 @@ export class Provider extends React.Component<ProviderProps> {
   }
 
   handleKeyDown: ConsumerWithKeyboardEventHandler = (consumer, event) => {
+    // cmd + left/right is for navigation in URL history
+    if (event.metaKey) return;
     const direction = getDirection(event.key);
     if (direction == null) return;
     this.move(direction);
