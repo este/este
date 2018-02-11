bef90c893b8a95e7812a85c34adf5335a2592050
Daniel Steigerwald <daniel@steigerwald.cz>
2018-02-11 23:18:31 +0100

    Refactor App to stateless functional component

--- components/app/index.js
+++ components/app/index.js
@@ -51,6 +51,17 @@ type NextProps = {
   },
 };
 
+// Stateless, because:
+// 1) Page should not have a state. Page state comes from URL or Relay.
+// 2) StatelessFunctionalComponent works well with HOC and Flow. Component not.
+// 3) We don't have to type props, they are inferred.
+type StatelessPage = React.StatelessFunctionalComponent<
+  {
+    data: Object,
+    intl: IntlShape,
+  } & NextProps,
+>;
+
 type InitialAppProps = {|
   cookie: ?Cookie,
   data: Object,
@@ -63,23 +74,31 @@ type InitialAppProps = {|
 
 type AppProps = NextProps & InitialAppProps;
 
-// Stateless, because:
-// 1) Page should not contain state. Page state comes from URL or GraphQL API.
-// 2) StatelessFunctionalComponent works well with HOC and Flow. Component not.
-// 3) We don't have to type props, they are inferred.
-type StatelessPage = React.StatelessFunctionalComponent<
-  {
-    data: Object,
-    intl: IntlShape,
-  } & NextProps,
->;
-
 export type QueryVariables<Query> = {|
   isAuthenticated: boolean,
   query: Query,
   userId: ?string,
 |};
 
+const redirectToSignIn = (context: NextContext) => {
+  const { asPath, res } = context;
+  const redirectUrlKey = 'redirectUrl';
+  const redirectUrl = encodeURIComponent(asPath);
+  const href: Href = {
+    pathname: '/sign-in',
+    query: { [redirectUrlKey]: redirectUrl },
+  };
+  if (res) {
+    res.writeHead(303, {
+      Location: `${href.pathname}?${redirectUrlKey}=${redirectUrl}`,
+    });
+    res.end();
+  } else {
+    Router.replace(href);
+  }
+};
+
+// https://reactjs.org/docs/higher-order-components.html
 const app = (
   Page: StatelessPage,
   options?: {|
@@ -91,127 +110,109 @@ const app = (
   const { query, queryVariables, requireAuth } = options || {};
   const PageWithHigherOrderComponents = injectIntl(Page);
 
-  class App extends React.PureComponent<AppProps> {
-    static redirectToSignIn = (context: NextContext) => {
-      const { asPath, res } = context;
-      const redirectUrlKey = 'redirectUrl';
-      const redirectUrl = encodeURIComponent(asPath);
-      const href: Href = {
-        pathname: '/sign-in',
-        query: { [redirectUrlKey]: redirectUrl },
-      };
-      if (res) {
-        res.writeHead(303, {
-          Location: `${href.pathname}?${redirectUrlKey}=${redirectUrl}`,
-        });
-        res.end();
-      } else {
-        Router.replace(href);
-      }
-    };
+  // Stateless, because App state belongs to providers.
+  const App = (props: AppProps) => {
+    const {
+      cookie,
+      data,
+      initialNow,
+      locale,
+      messages,
+      records,
+      supportedLocales,
+      url,
+    } = props;
 
-    static getInitialProps = async (context: NextContext) => {
-      const cookie = getCookie(context.req);
-      const isAuthenticated = !!cookie;
+    const token = cookie && cookie.token;
+    const environment = createRelayEnvironment(token, records);
+    const userId = cookie && cookie.userId;
+    const isAuthenticated = !!cookie;
+    const variables = queryVariables
+      ? queryVariables({
+          isAuthenticated,
+          query: url.query,
+          userId,
+        })
+      : {};
 
-      if (requireAuth === true && !isAuthenticated) {
-        App.redirectToSignIn(context);
-        // Return nothing because component will not be rendered on redirect.
-        return {};
-      }
+    return (
+      <IntlProvider
+        locale={locale}
+        messages={messages}
+        initialNow={initialNow}
+        // https://github.com/yahoo/react-intl/issues/999#issuecomment-335799491
+        textComponent={({ children }) => children}
+      >
+        <LocaleProvider value={{ locale, supportedLocales }}>
+          <RelayProvider environment={environment} variables={variables}>
+            <IsAuthenticatedProvider value={{ isAuthenticated, userId }}>
+              <ErrorPopupProvider>
+                <PageWithHigherOrderComponents data={data} url={url} />
+              </ErrorPopupProvider>
+            </IsAuthenticatedProvider>
+          </RelayProvider>
+        </LocaleProvider>
+      </IntlProvider>
+    );
+  };
 
-      let data = {};
-      let records = {};
+  // SSR data fetching with pending navigation FTW.
+  App.getInitialProps = async (context: NextContext) => {
+    const cookie = getCookie(context.req);
+    const isAuthenticated = !!cookie;
 
-      // Note we call fetchQuery for client page transitions as well to enable
-      // pending navigations. Finally possible with Next.js and Relay.
-      // https://writing.pupius.co.uk/beyond-pushstate-building-single-page-applications-4353246f4480
-      if (query) {
-        const environment = createRelayEnvironment(cookie && cookie.token);
-        const variables = queryVariables
-          ? queryVariables({
-              isAuthenticated,
-              query: context.query,
-              userId: cookie && cookie.userId,
-            })
-          : {};
-        // It can throw "Failed to fetch" error when offline, but it should be
-        // solved with service workers I believe.
-        // It does not throw on payload errors like insufficient permissions etc.,
-        // because payload errors are not real errors. They are expected when the
-        // schema is updated and an app is not yet updated. That's why Relay
-        // generated Flow types are optional. Don't crash, just don't show data.
-        // Another mechanism should invoke app update.
-        data = await fetchQuery(environment, query, variables);
-        records = environment
-          .getStore()
-          .getSource()
-          .toJSON();
-      }
+    if (requireAuth === true && !isAuthenticated) {
+      redirectToSignIn(context);
+      // Return nothing because component will not be rendered on redirect.
+      return {};
+    }
 
-      // Always update the current time on page load/transition because the
-      // <IntlProvider> will be a new instance even with pushState routing.
-      const initialNow = Date.now();
+    let data = {};
+    let records = {};
 
-      const { locale, messages, supportedLocales } =
-        context.req || window.__NEXT_DATA__.props;
-
-      return ({
-        cookie,
-        data,
-        initialNow,
-        locale,
-        messages,
-        records,
-        supportedLocales,
-      }: InitialAppProps);
-    };
-
-    render() {
-      const {
-        cookie,
-        data,
-        initialNow,
-        locale,
-        messages,
-        records,
-        supportedLocales,
-        url,
-      } = this.props;
-
-      const token = cookie && cookie.token;
-      const environment = createRelayEnvironment(token, records);
-      const userId = cookie && cookie.userId;
-      const isAuthenticated = !!cookie;
+    // Note we call fetchQuery for client page transitions as well to enable
+    // pending navigations. Finally possible with Next.js and Relay.
+    // https://writing.pupius.co.uk/beyond-pushstate-building-single-page-applications-4353246f4480
+    if (query) {
+      const environment = createRelayEnvironment(cookie && cookie.token);
       const variables = queryVariables
         ? queryVariables({
             isAuthenticated,
-            query: url.query,
-            userId,
+            query: context.query,
+            userId: cookie && cookie.userId,
           })
         : {};
-
-      return (
-        <IntlProvider
-          locale={locale}
-          messages={messages}
-          initialNow={initialNow}
-          // https://github.com/yahoo/react-intl/issues/999#issuecomment-335799491
-          textComponent={({ children }) => children}
-        >
-          <LocaleProvider value={{ locale, supportedLocales }}>
-            <RelayProvider environment={environment} variables={variables}>
-              <IsAuthenticatedProvider value={{ isAuthenticated, userId }}>
-                <ErrorPopupProvider>
-                  <PageWithHigherOrderComponents data={data} url={url} />
-                </ErrorPopupProvider>
-              </IsAuthenticatedProvider>
-            </RelayProvider>
-          </LocaleProvider>
-        </IntlProvider>
-      );
+      // It can throw "Failed to fetch" error when offline, but it should be
+      // solved with service workers I believe.
+      // It does not throw on payload errors like insufficient permissions etc.,
+      // because payload errors are not real errors. They are expected when the
+      // schema is updated and an app is not yet updated. That's why Relay
+      // generated Flow types are optional. Don't crash, just don't show data.
+      // Another mechanism should invoke app update.
+      data = await fetchQuery(environment, query, variables);
+      records = environment
+        .getStore()
+        .getSource()
+        .toJSON();
     }
-  }
+
+    // Always update the current time on page load/transition because the
+    // <IntlProvider> will be a new instance even with pushState routing.
+    const initialNow = Date.now();
+
+    const { locale, messages, supportedLocales } =
+      context.req || window.__NEXT_DATA__.props;
+
+    return ({
+      cookie,
+      data,
+      initialNow,
+      locale,
+      messages,
+      records,
+      supportedLocales,
+    }: InitialAppProps);
+  };
 
   return App;
 };
