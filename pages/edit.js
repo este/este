a58bb5930d0cfe9976db8f650120b11efbbe99e4
Daniel Steigerwald <daniel@steigerwald.cz>
2018-02-27 23:37:27 +0100

    Restore edit

--- pages/edit.js
+++ pages/edit.js
@@ -3,17 +3,14 @@ import * as React from 'react';
 import app from '../components/app';
 import { graphql } from 'react-relay';
 import * as generated from './__generated__/editQuery.graphql';
-import type { EditQuery } from '../server/sitemap';
 import Error from 'next/error';
 import Editor from '../components/editor/Editor';
 
-const Edit = () => null;
-
-// const Edit = props => {
-//   const { web }: generated.editQueryResponse = props.data;
-//   if (!web) return <Error statusCode={404} />;
-//   return <Editor name={web.name} />;
-// };
+const Edit = props => {
+  const { web }: generated.editQueryResponse = props.data;
+  if (!web) return <Error statusCode={404} />;
+  return <Editor name={web.name} />;
+};
 
 export default app(Edit, {
   query: graphql`
