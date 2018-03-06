b3caf8a0353cb4ee3ad700e1888eb612d5354eaa
Daniel Steigerwald <daniel@steigerwald.cz>
2018-03-06 01:37:57 +0100

    Close #1490

--- next.config.js
+++ next.config.js
@@ -21,11 +21,6 @@ module.exports = {
       enforce: 'pre',
       exclude: /node_modules/,
       loader: 'eslint-loader',
-      options: {
-        // Emit errors as warnings for dev to not break webpack build.
-        // Eslint errors are shown in console for dev, yay :-)
-        emitWarning: dev,
-      },
     });
 
     return config;
