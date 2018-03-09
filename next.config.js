c75714b3b618eef325fa5db1188fe03bab3b6201
Daniel Steigerwald <daniel@steigerwald.cz>
2018-03-09 05:29:34 +0100

    OK, it works again.

--- next.config.js
+++ next.config.js
@@ -21,6 +21,11 @@ module.exports = {
       enforce: 'pre',
       exclude: /node_modules/,
       loader: 'eslint-loader',
+      options: {
+        // Emit errors as warnings for dev to not break webpack build.
+        // Eslint errors are shown in console for dev, yay :-)
+        emitWarning: dev,
+      },
     });
 
     return config;
