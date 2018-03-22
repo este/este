e266e0d89651a19982a423702b663e7146d341d2
Daniel Steigerwald <daniel@steigerwald.cz>
2018-03-22 02:59:39 +0100

    Form

--- components/core/Auth.js
+++ components/core/Auth.js
@@ -1,12 +1,12 @@
 // @flow
 import * as React from 'react';
-import Box from './Box';
-import Form from './Form';
 import Heading from './Heading';
-import Set from './Set';
-import TextInputBig from './TextInputBig';
-import Error from './Error';
-import { SignInButton, SignUpButton } from './buttons';
+import Form from './Form';
+import TextInput from './TextInput';
+// import Set from './Set';
+// import TextInputBig from './TextInputBig';
+// import Error from './Error';
+// import { SignInButton, SignUpButton } from './buttons';
 import SigninMutation from '../../mutations/SigninMutation';
 import SignupMutation from '../../mutations/SignupMutation';
 import Router from 'next/router';
@@ -109,62 +109,59 @@ class Auth extends React.PureComponent<AuthProps, AuthState> {
     return (
       <Mutation>
         {({ mutate, pending }) => (
-          <div>
-            {/* https://stackoverflow.com/questions/2781549/removing-input-background-colour-for-chrome-autocomplete/32505530#32505530 */}
-            <style jsx>{`
-              div :global(input:-webkit-autofill),
-              div :global(input:-webkit-autofill:hover),
-              div :global(input:-webkit-autofill:focus),
-              div :global(input:-webkit-autofill:active) {
-                -webkit-transition: 'color 9999s ease-out, background-color 9999s ease-out';
-                -webkit-transition-delay: 9999s;
-              }
-            `}</style>
-            <Box>
-              <Heading size={3}>Auth</Heading>
-              <Form onSubmit={this.auth(mutate)}>
+          <React.Fragment>
+            <Heading size={3}>Auth</Heading>
+            <Form onSubmit={this.auth(mutate)}>
+              <Heading>Auth</Heading>
+              <TextInput
+                name="email"
+                placeholder="fok"
+                type="email"
+                maxLength={6}
+              />
+              {/*
                 <Set vertical spaceBetween={0}>
-                  <TextInputBig
-                    autoFocus={this.state.errors.email}
-                    disabled={pending}
-                    error={<Error>{this.state.errors.email}</Error>}
-                    maxWidth={26}
-                    name="email"
-                    onChange={email => this.setState({ email })}
-                    placeholder={this.props.intl.formatMessage(
-                      messages.emailPlaceholder,
-                    )}
-                    type="email"
-                    value={this.state.email}
-                  />
-                  <TextInputBig
-                    autoFocus={this.state.errors.password}
-                    disabled={pending}
-                    error={<Error>{this.state.errors.password}</Error>}
-                    maxWidth={26}
-                    name="password"
-                    onChange={password => this.setState({ password })}
-                    placeholder={this.props.intl.formatMessage(
-                      messages.passwordPlaceholder,
-                    )}
-                    type="password"
-                    value={this.state.password}
-                  />
+                <TextInputBig
+                autoFocus={this.state.errors.email}
+                disabled={pending}
+                error={<Error>{this.state.errors.email}</Error>}
+                maxWidth={26}
+                name="email"
+                onChange={email => this.setState({ email })}
+                placeholder={this.props.intl.formatMessage(
+                messages.emailPlaceholder,
+                )}
+                type="email"
+                value={this.state.email}
+                />
+                <TextInputBig
+                autoFocus={this.state.errors.password}
+                disabled={pending}
+                error={<Error>{this.state.errors.password}</Error>}
+                maxWidth={26}
+                name="password"
+                onChange={password => this.setState({ password })}
+                placeholder={this.props.intl.formatMessage(
+                messages.passwordPlaceholder,
+                )}
+                type="password"
+                value={this.state.password}
+                />
                 </Set>
                 <Set>
-                  <SignInButton
-                    disabled={pending}
-                    onPress={this.auth(mutate)}
-                    primary
-                  />
-                  <SignUpButton
-                    disabled={pending}
-                    onPress={this.auth(mutate, true)}
-                  />
+                <SignInButton
+                disabled={pending}
+                onPress={this.auth(mutate)}
+                primary
+                />
+                <SignUpButton
+                disabled={pending}
+                onPress={this.auth(mutate, true)}
+                />
                 </Set>
-              </Form>
-            </Box>
-          </div>
+              */}
+            </Form>
+          </React.Fragment>
         )}
       </Mutation>
     );
--- components/core/Form.js
+++ components/core/Form.js
@@ -1,19 +1,35 @@
 // @flow
 import * as React from 'react';
-import Box, { type BoxProps } from './Box';
 import Theme from './Theme';
+import { View } from 'react-native';
 
-// Render form as form in browser, because auth data or whatever pre-filling.
-type BrowserFormProps = {|
-  onSubmit: () => void,
+// TODO: Rename to Form.web.js, add native Form.js.
+
+// https://stackoverflow.com/questions/2781549/removing-input-background-colour-for-chrome-autocomplete/32505530#32505530
+const DisableWebkitAutofillColor = () => (
+  <style jsx global>{`
+    input:-webkit-autofill,
+    input:-webkit-autofill:hover,
+    input:-webkit-autofill:focus,
+    input:-webkit-autofill:active {
+      -webkit-transition: 'color 9999s ease-out, background-color 9999s ease-out';
+      -webkit-transition-delay: 9999s;
+    }
+  `}</style>
+);
+
+type FormProps = {|
+  children?: React.Node,
+  onSubmit?: () => void,
 |};
 
-class BrowserForm extends React.PureComponent<BrowserFormProps> {
+class Form extends React.PureComponent<FormProps> {
   static handleSubmit = (e: Event) => {
     e.preventDefault();
   };
 
   handleKeyPress = (e: KeyboardEvent) => {
+    // Submit on any input key enter.
     if (e.target.tagName !== 'INPUT') return;
     if (e.key !== 'Enter') return;
     if (typeof this.props.onSubmit !== 'function') return;
@@ -22,40 +38,18 @@ class BrowserForm extends React.PureComponent<BrowserFormProps> {
 
   render() {
     return (
-      // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
-      <form
-        {...this.props}
-        onKeyPress={this.handleKeyPress}
-        onSubmit={BrowserForm.handleSubmit}
-      />
-    );
-  }
-}
-
-type FormProps = {
-  onSubmit?: () => any,
-} & BoxProps;
-
-class Form extends React.PureComponent<FormProps> {
-  render() {
-    return (
       <Theme>
-        {theme => {
-          const {
-            as = BrowserForm,
-            marginBottom = theme.form.marginBottom,
-            maxWidth = theme.form.maxWidth,
-            ...props
-          } = this.props;
-          return (
-            <Box
-              as={as}
-              marginBottom={marginBottom}
-              maxWidth={maxWidth}
-              {...props}
-            />
-          );
-        }}
+        {theme => (
+          <View
+            accessibilityRole="form"
+            style={theme.styles.form.view}
+            onSubmit={Form.handleSubmit}
+            onKeyPress={this.handleKeyPress}
+          >
+            <DisableWebkitAutofillColor />
+            {this.props.children}
+          </View>
+        )}
       </Theme>
     );
   }
