f2a4662a390bdd6912d16cf0e25201a491a0a6c6
Daniel Steigerwald <daniel@steigerwald.cz>
2018-04-24 23:37:41 +0200

    Warn if .env file is missing

--- scripts/babel.js
+++ scripts/babel.js
@@ -2,6 +2,10 @@
 // https://github.com/motdotla/dotenv
 const fs = require('fs');
 
+if (fs.existsSync('.env') === false) {
+  throw new Error('Missing .env file. Run "yarn env dev" to make one.');
+}
+
 // `yarn babel web` will copy `.babelrc.web` file to `.babelrc` file.
 const name = process.argv[process.argv.length - 1];
 
