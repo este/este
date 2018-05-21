80387c7971bc5a29f2f09369c1c8323eb4ded8ac
Daniel Steigerwald <steida@Daniels-MacBook-Pro.local>
2018-05-21 16:14:55 +0200

    Update old yarn message

--- scripts/babel.mjs
+++ scripts/babel.mjs
@@ -3,7 +3,7 @@
 import fs from 'fs';
 
 if (fs.existsSync('.env') === false) {
-  throw new Error('Missing .env file. Run "yarn env dev" to make one.');
+  throw new Error('Missing .env file. Run "npm run env dev" to make one.');
 }
 
 // `npm run babel web` will copy `.babelrc.web` file to `.babelrc` file.
