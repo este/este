efc395a4e391fdc485c4b0ec10e9ee82e4b28059
Daniel Steigerwald <steida@Daniels-MacBook-Pro.local>
2018-05-22 22:25:21 +0200

    Remove dead component

--- components/core/CountryFlag.js
+++
@@ -1,7 +0,0 @@
-// @flow
-const CountryFlag = ({ code }: { code: string }) =>
-  code
-    .toUpperCase()
-    .replace(/./g, char => String.fromCodePoint(char.charCodeAt(0) + 127397));
-
-export default CountryFlag;
