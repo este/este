ff21b3570a4bfb5a3f94114d0eca4827fe7c8fea
Daniel Steigerwald <steida@Daniels-MacBook-Pro.local>
2018-05-22 21:36:58 +0200

    npm run analyze:bundles, npm run analyze:size

--- next.config.js
+++ next.config.js
@@ -1,16 +1,24 @@
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const {
+  WebpackBundleSizeAnalyzerPlugin,
+} = require('webpack-bundle-size-analyzer');
 const { ANALYZE } = process.env;
 
 module.exports = {
   webpack: function(config, { isServer }) {
-    if (ANALYZE) {
-      config.plugins.push(
-        new BundleAnalyzerPlugin({
-          analyzerMode: 'server',
-          analyzerPort: isServer ? 8888 : 8889,
-          openAnalyzer: true,
-        }),
-      );
+    switch (ANALYZE) {
+      case 'BUNDLES':
+        config.plugins.push(
+          new BundleAnalyzerPlugin({
+            analyzerMode: 'server',
+            analyzerPort: isServer ? 8888 : 8889,
+            openAnalyzer: true,
+          }),
+        );
+        break;
+      case 'SIZE':
+        config.plugins.push(new WebpackBundleSizeAnalyzerPlugin('stats.txt'));
+        break;
     }
 
     return config;
