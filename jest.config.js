af353b77a8581d69518aee4bccf9aac75699d90c
Daniel Steigerwald <steida@Daniels-MacBook-Pro.local>
2018-07-28 21:30:53 +0200

    Fix build

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   setupFiles: ['raf/polyfill'],
   testPathIgnorePatterns: ['/node_modules/', '.next'],
+  // https://stackoverflow.com/a/51554619/233902
+  testURL: 'http://localhost/',
 };
