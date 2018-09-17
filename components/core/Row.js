ed751f49a88731f5dbc1818b69e55079bf3ae09d
Daniel Steigerwald <steida@Daniels-MacBook-Pro.local>
2018-09-17 23:47:09 +0200

    Add wrap prop to Row component

--- components/core/Row.js
+++ components/core/Row.js
@@ -6,19 +6,23 @@ import Spacer from './Spacer';
 type Props = {|
   children: React.Node,
   rhythm?: number,
+  wrap?: boolean,
 |};
 
 const styles = StyleSheet.create({
   view: {
     flexDirection: 'row',
   },
+  wrap: {
+    flexWrap: 'wrap',
+  },
 });
 
 class Row extends React.PureComponent<Props> {
   render() {
-    const { children, rhythm } = this.props;
+    const { children, rhythm, wrap } = this.props;
     return (
-      <View style={styles.view}>
+      <View style={[styles.view, wrap === true && styles.wrap]}>
         <Spacer rhythm={rhythm}>{children}</Spacer>
       </View>
     );
