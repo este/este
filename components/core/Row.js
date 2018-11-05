0ef76b5c4a82703308dc68672418483a413c4b01
Daniel Steigerwald <steida@Daniels-MacBook-Pro.local>
2018-11-05 20:47:58 +0100

    Refactor Row and Spacer

--- components/core/Row.js
+++ components/core/Row.js
@@ -1,13 +1,7 @@
 // @flow
-import * as React from 'react';
+import React, { type Node } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Spacer from './Spacer';
-
-type Props = {|
-  children: React.Node,
-  rhythm?: number,
-  wrap?: boolean,
-|};
 
 const styles = StyleSheet.create({
   view: {
@@ -18,15 +12,14 @@ const styles = StyleSheet.create({
   },
 });
 
-class Row extends React.PureComponent<Props> {
-  render() {
-    const { children, rhythm, wrap } = this.props;
-    return (
-      <View style={[styles.view, wrap === true && styles.wrap]}>
-        <Spacer rhythm={rhythm}>{children}</Spacer>
-      </View>
-    );
-  }
+export default function Row(props: {|
+  children: Node,
+  rhythm?: number,
+  wrap?: boolean,
+|}) {
+  return (
+    <View style={[styles.view, props.wrap === true && styles.wrap]}>
+      <Spacer rhythm={props.rhythm}>{props.children}</Spacer>
+    </View>
+  );
 }
-
-export default Row;
--- components/core/Spacer.js
+++ components/core/Spacer.js
@@ -1,7 +1,7 @@
 // @flow
-import * as React from 'react';
+import React, { type Node } from 'react';
 import { View } from 'react-native';
-import withTheme, { type Theme } from './withTheme';
+import useTheme from './useTheme';
 
 // Put some space between those components for God's sake!
 // The most sane approach I guess. Can't believe CSS ignores this use case.
@@ -9,29 +9,20 @@ import withTheme, { type Theme } from './withTheme';
 // https://reactjs.org/docs/jsx-in-depth.html#booleans-null-and-undefined-are-ignored
 const visible = value => !(value == null || typeof value === 'boolean');
 
-type Props = {|
-  children: React.Node,
-  theme: Theme,
-  rhythm?: number,
-|};
+export default function Spacer(props: {| children: Node, rhythm?: number |}) {
+  const theme = useTheme();
 
-class Spacer extends React.PureComponent<Props> {
-  render() {
-    const { children, theme, rhythm = 1 } = this.props;
-    return React.Children.toArray(children)
-      .filter(visible)
-      .reduce((spaced, child, index, array) => {
-        if (index === array.length - 1) return [...spaced, child];
-        const spacer = (
-          <View
-            // eslint-disable-next-line react/no-array-index-key
-            key={`spacer-${index}`}
-            style={{ width: theme.typography.rhythm(rhythm) }}
-          />
-        );
-        return [...spaced, child, spacer];
-      }, []);
-  }
+  return React.Children.toArray(props.children)
+    .filter(visible)
+    .reduce((spaced, child, index, array) => {
+      if (index === array.length - 1) return [...spaced, child];
+      const spacer = (
+        <View
+          // eslint-disable-next-line react/no-array-index-key
+          key={`spacer-${index}`}
+          style={{ width: theme.typography.rhythm(props.rhythm ?? 1) }}
+        />
+      );
+      return [...spaced, child, spacer];
+    }, []);
 }
-
-export default withTheme(Spacer);
