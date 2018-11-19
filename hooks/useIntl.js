cae2a2adbd60a657f3ea573c80b730d69448b800
Daniel Steigerwald <steida@Daniels-MacBook-Pro.local>
2018-11-19 20:03:36 +0100

    Remove unnecessary types
    Typed useContext extracts them from Context.

--- hooks/useIntl.js
+++ hooks/useIntl.js
@@ -1,9 +1,8 @@
 // @flow
 import { useContext } from 'react';
-import { type IntlShape } from 'react-intl';
 import IntlContext from '../components/core/IntlContext';
 
-export default function useIntl(): IntlShape {
+export default function useIntl() {
   const context = useContext(IntlContext);
   if (context == null) throw Error('useIntl: Please provide IntlContext.');
   return context;
--- hooks/useTheme.js
+++ hooks/useTheme.js
@@ -1,8 +1,7 @@
 // @flow
 import { useContext } from 'react';
 import ThemeContext from '../components/core/ThemeContext';
-import type { Theme } from '../themes/types';
 
-export default function useTheme(): Theme {
+export default function useTheme() {
   return useContext(ThemeContext);
 }
