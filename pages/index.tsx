12f9b093771ec51cd67c83a082d3841b596fe1dd
Daniel Steogerwald <daniel@steigerwald.cz>
2019-01-25 20:27:38 +0100

    Update index.tsx
    Ups.

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,7 +62,6 @@ const NotAuthenticated: React.FunctionComponent = () => {
   return (
     <>
       <Text style={theme.heading1}>{title}</Text>
-      <Text style={theme.heading1}>test</Text>
       <Text style={theme.paragraph}>
         <Link href={{ pathname: '/signin' }}>Create Web</Link>
       </Text>
