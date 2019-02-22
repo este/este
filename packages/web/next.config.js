9e17b03425d58fe054cd4c1d4fe08b7b3cc70a62
Daniel Steogerwald <daniel@steigerwald.cz>
2019-02-22 20:32:28 +0100

    Use next-transpile-modules instead of next-plugin-transpile-modules

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 const withCustomBabelConfig = require('next-plugin-custom-babel-config');
-const withTranspileModules = require('next-plugin-transpile-modules');
+const withTranspileModules = require('next-transpile-modules');
 const withTypescript = require('@zeit/next-typescript');
 
 const { BUNDLE_ANALYZE } = process.env;
