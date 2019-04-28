25dc7d0d3ca46703da7d088b8fcd971858a6610c
Daniel Steogerwald <daniel@steigerwald.cz>
2019-04-29 00:55:57 +0200

    Close #1625

--- packages/web/pages/_document.tsx
+++ packages/web/pages/_document.tsx
@@ -41,6 +41,25 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {/* https://favicon.io/favicon-generator/ */}
+          {/* Lemonada, #228be6, transparent */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicon-16x16.png"
+          />
         </Head>
         <body>
           <Main />
