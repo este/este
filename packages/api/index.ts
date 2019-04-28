bd5d34fdc8dfffa561d8cbd3ed5290f88f59c8e0
Daniel Steogerwald <daniel@steigerwald.cz>
2019-04-28 21:28:09 +0200

    Add micro-cors to api
    It fixes a bug.

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from 'apollo-server-micro';
 import { makeSchema } from 'nexus';
 import path from 'path';
 import { createServer } from 'http';
+import cors from 'micro-cors';
 import { prisma } from '../../prisma/generated/prisma-client';
 import * as schemaTypes from './schema';
 import { Context } from './types';
@@ -40,8 +41,19 @@ const server = new ApolloServer({
 // Set in now.json. Unfortunately, it does not work for next.config.js.
 const { IS_NOW } = process.env;
 
-const handler = server.createHandler({
+const serverHandler = server.createHandler({
   path: IS_NOW ? '/api' : '/',
+});
+
+const handler = cors()((req, res) => {
+  // TODO: Investigate why this weird piece of code is required.
+  // https://github.com/apollographql/apollo-server/issues/2362
+  // https://github.com/apollographql/apollo-server/issues/2473
+  if (req.method === 'OPTIONS') {
+    res.end();
+    return;
+  }
+  return serverHandler(req, res);
 });
 
 if (!process.env.IS_NOW) {
